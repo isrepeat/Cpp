9dd791d64df19aa2fb99a1657607d236f08e65b0
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-01-04 17:45:23 +0200

    Add WinRT_DLL project

---
+++ WinRT_DLL/Class1.cpp
@@ -0,0 +1,8 @@
+﻿#include "Class1.h"
+
+using namespace WinRT_DLL;
+using namespace Platform;
+
+Class1::Class1()
+{
+}
---
+++ WinRT_DLL/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace WinRT_DLL
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+    };
+}
