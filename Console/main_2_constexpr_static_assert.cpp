a7f3bbca638329decde849ada0b2e44e6f85639b
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-01-16 22:24:49 +0200

    [Console proj] Add template_Vargs_to_array

--- Console/main_2_constexpr_static_assert.cpp
+++ Console/main_2_constexpr_static_assert.cpp
@@ -1,22 +1,22 @@
-#include <iostream>
-
-template <typename T>
-void Foo(T str) {
-	if constexpr (std::is_constructible_v<std::string, T>) {
-		static_assert(false, "This is std::string");
-	}
-	else if constexpr (std::is_constructible_v<std::wstring, T>) {
-		static_assert(false, "This is std::wstring");
-	}
-	else {
-		static_assert(false, "Bad arg");
-	}
-}
-
-
-int main() {
-
-	Foo(L"");
-
-	return 0;
-}
+//#include <iostream>
+//
+//template <typename T>
+//void Foo(T str) {
+//	if constexpr (std::is_constructible_v<std::string, T>) {
+//		static_assert(false, "This is std::string");
+//	}
+//	else if constexpr (std::is_constructible_v<std::wstring, T>) {
+//		static_assert(false, "This is std::wstring");
+//	}
+//	else {
+//		static_assert(false, "Bad arg");
+//	}
+//}
+//
+//
+//int main() {
+//
+//	Foo(L"");
+//
+//	return 0;
+//}
