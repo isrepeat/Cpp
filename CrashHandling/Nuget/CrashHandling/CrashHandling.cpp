b78c0cdbbd0f9cd5f7f7c42e822b004b20b15791
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-01-17 20:24:00 +0200

    Add code to nuget, change hierarchy, add msbuild tasks

---
+++ CrashHandling/Nuget/CrashHandling/CrashHandling.cpp
@@ -0,0 +1,53 @@
+#include "CrashHandling.h"
+#include "MiniDumpMessages.h"
+#include "../../../Shared/Helpers/Helpers.h"
+#include "../../../Shared/Helpers/Channel.h"
+#include "../../../Shared/Helpers/CrashInfo.h"
+#include <filesystem>
+#include <dbghelp.h>
+#pragma comment (lib, "dbghelp.lib" )
+
+
+namespace CrashHandling {
+	Channel<MiniDumpMessages> channelMinidump;
+
+	API void OpenMinidumpChannel(EXCEPTION_POINTERS* pep, std::wstring packageFolder) {
+
+		auto hProcess = GetCurrentProcess();
+		auto processId = GetCurrentProcessId();
+		auto threadId = GetCurrentThreadId();
+
+		try {
+			channelMinidump.Open(L"\\\\.\\pipe\\Local\\channelDumpWriter", [hProcess, processId, threadId, pep, packageFolder](Channel<MiniDumpMessages>::ReadFunc Read, Channel<MiniDumpMessages>::WriteFunc Write) {
+				auto reply = Read();
+				switch (reply.type)
+				{
+				case MiniDumpMessages::Connect: {
+					//auto strData = std::to_string(threadId);
+					//Write({ strData.begin(), strData.end() }, MiniDumpMessages::ThreadId);
+
+					auto strData = H::WStrToStr(packageFolder);
+					Write({ strData.begin(), strData.end() }, MiniDumpMessages::PackageFolder);
+
+					CrashInfo crashInfo;
+					//crashInfo.hProcess = hProcess;
+					//crashInfo.processId = processId;
+					crashInfo.threadId = threadId;
+					FillCrashInfoWithExceptionPointers(crashInfo, pep);
+					auto serializedData = SerializeCrashInfo(crashInfo);
+					Write(std::move(serializedData), MiniDumpMessages::ExceptionInfo);
+					break;
+				}
+				}
+
+				return true;
+				}, 30'000);
+
+			channelMinidump.WaitFinishSendingMessage(MiniDumpMessages::ExceptionInfo);
+			int xxx = 9;
+		}
+		catch (...) {
+			int xxx = 9;
+		}
+	}
+}
--- CrashHandling/Nuget/CrashHandling/CrashHandling.h
+++ CrashHandling/Nuget/CrashHandling/CrashHandling.h
@@ -1,1 +1,17 @@
 #pragma once
+#include <string>
+#include <functional>
+#include <Windows.h>
+
+#ifdef __MAKE_DLL__
+#define API __declspec(dllexport)
+#else
+//#define API // for static
+#define API __declspec(dllimport) // for dll
+#endif
+
+
+namespace CrashHandling {
+	API void RegisterVectorHandler(PVECTORED_EXCEPTION_HANDLER handler);
+	API void OpenMinidumpChannel(EXCEPTION_POINTERS* pep, std::wstring packageFolder);
+}
