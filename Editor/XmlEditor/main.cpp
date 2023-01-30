794b0ab9dee31fbc766ccf902da307238330622c
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-01-30 07:44:03 +0200

    Use command args

--- Editor/XmlEditor/main.cpp
+++ Editor/XmlEditor/main.cpp
@@ -1,3 +1,4 @@
+#include <Windows.h>
 #include <filesystem>
 #include <functional>
 #include <iostream>
@@ -9,6 +10,11 @@
 #include <queue>
 #include <utility>
 
+#define TEST_WITHOUT_ARGS 1
+
+#ifndef _DEBUG
+#define TEST_WITHOUT_ARGS 0
+#endif
 
 void ReadFileByLine(const std::wstring& inputFilename, std::function<void(std::wstring)> callback) {
 	std::wifstream inFile(inputFilename);
@@ -55,25 +61,58 @@ void WriteFile(const std::wstring& outputFilename, const std::vector<std::wstrin
 }
 
 
-int main() {
-	std::list<std::pair<std::wstring, std::wstring>> listReplacedStrings;
 
-	ReadFileByLine(L"Original.txt", [&listReplacedStrings] (std::wstring originalString) {
-		listReplacedStrings.push_back({ originalString, L"" });
-		});
+int wmain(int argc, wchar_t* argv[]) {
+#if TEST_WITHOUT_ARGS == 1
+	std::wstring originalFilename = L"rd_manager_ru.ts";
+	std::wstring outputFilename = L"out.txt";
+	std::wstring originalStringsFilename = L"OriginalStrings.txt";
+	std::wstring replacedStringsFilename = L"ReplacedStrings_ru.txt";
+#else
+	for (int i = 0; i < argc; i++) {
+		wprintf(L"arg [%d] = %s \n", i, argv[i]);
+	}
+	wprintf(L"\n\n");
+
+	if (argc != 3 && argc != 5) {
+		wprintf(L"Usage \"XmlEditor.exe <Original.xml> <Output.xml> \"\n");
+		wprintf(L"or \n");
+		wprintf(L"Usage \"XmlEditor.exe <Original.xml> <Output.xml> <OriginalStrings.txt> <ReplacedStrings.txt> \"\n");
+		Sleep(1000);
+		return 1;
+	}
+
+	std::wstring originalFilename = argv[1];
+	std::wstring outputFilename = argv[2];
+	std::wstring originalStringsFilename = argc > 3 ? argv[3] : L"OriginalStrings.txt";
+	std::wstring replacedStringsFilename = argc > 3 ? argv[4] : L"ReplacedStrings.txt";
+#endif
+
+	try {
+		std::list<std::pair<std::wstring, std::wstring>> listReplacedStrings;
+
+		ReadFileByLine(originalStringsFilename, [&listReplacedStrings](std::wstring originalString) {
+			listReplacedStrings.push_back({ originalString, L"" });
+			});
 
 
-	auto it = listReplacedStrings.begin();
-	ReadFileByLine(L"Replaced.txt", [&it, &listReplacedStrings](std::wstring replacedString) {
-		if (it != listReplacedStrings.end()) {
-			it->second = replacedString;
-			it++;
-		}
-		});
+		auto it = listReplacedStrings.begin();
+		ReadFileByLine(replacedStringsFilename, [&it, &listReplacedStrings](std::wstring replacedString) {
+			if (it != listReplacedStrings.end()) {
+				it->second = replacedString;
+				it++;
+			}
+			});
 
 
-	auto lines = ReadFileWithReplacedLines(L"Test.xml.txt", listReplacedStrings);
-	WriteFile(L"TestOut.xml.txt", lines);
-
+		auto lines = ReadFileWithReplacedLines(originalFilename, listReplacedStrings);
+		WriteFile(outputFilename, lines);
+	}
+	catch (...) {
+		wprintf(L"\n\n");
+		wprintf(L"was exception ... \n");
+		Sleep(1000);
+		return 1;
+	}
 	return 0;
 }
