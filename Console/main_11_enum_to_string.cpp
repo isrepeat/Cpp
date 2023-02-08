89d7c448d74d5306d0d46e65b9de1a4368f1477a
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-02-08 15:12:42 +0200

    [Console] Add main_11_enum_to_string

---
+++ Console/main_11_enum_to_string.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include "main_11_enum_to_string.h"
+
+#define TO_STRING(x) #x
+
+enum class HTTPErrors {
+	Unauthorized = 401,
+	Forbidden = 403,
+	Timeout = 405,
+};
+
+int main() {
+	std::string text = std::string("Error is ") + TO_STRING(HTTPErrors::Unauthorized);
+
+	Foo(2);
+
+	return 0;
+}
