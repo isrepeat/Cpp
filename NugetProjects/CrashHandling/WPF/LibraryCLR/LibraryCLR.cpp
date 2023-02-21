08e70859a2900778928ea847bc0a8db8dee04e22
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-02-21 15:04:54 +0200

    Readd LibraryCLR as empty project, add msbuild tasks for copying dlls

--- NugetProjects/CrashHandling/WPF/LibraryCLR/LibraryCLR.cpp
+++
@@ -1,9 +0,0 @@
-#include "LibraryCLR.h"
-//#include <Windows.h>
-
-namespace LibraryCLR {
-	TestClass::TestClass() {
-		//*(int*)0 = 0;
-		//Sleep(1000);
-	}
-}
--- NugetProjects/CrashHandling/WPF/LibraryCLR/TestClass.h
+++ NugetProjects/CrashHandling/WPF/LibraryCLR/TestClass.h
@@ -1,10 +1,8 @@
 #pragma once
 
-using namespace System;
 
 namespace LibraryCLR {
-	public ref class TestClass
-	{
+	public ref class TestClass {
 	public:
 		TestClass();
 	};
