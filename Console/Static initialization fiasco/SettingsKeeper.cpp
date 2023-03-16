ffc2752da1813046ce75b42e6cd1a40d7959173b
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-03-16 13:47:07 +0200

    Add main_18_detect_windows_11

--- Console/Static initialization fiasco/SettingsKeeper.cpp
+++ Console/Static initialization fiasco/SettingsKeeper.cpp
@@ -1,29 +1,29 @@
-#include "SettingsKeeper.h"
-#include <string>
-
-namespace {
-	const std::string uuidAppKey = "uuidAppKey";
-}
-//const std::wstring& SettingsKeeper::uuidAppKey() {
-//	static std::wstring uuidAppKey = L"uuidAppKey";
-//	return uuidAppKey;
+//#include "SettingsKeeper.h"
+//#include <string>
+//
+//namespace {
+//	const std::string uuidAppKey = "uuidAppKey";
 //}
-
-//SettingsKeeper::SettingsKeeper()
-//{
+////const std::wstring& SettingsKeeper::uuidAppKey() {
+////	static std::wstring uuidAppKey = L"uuidAppKey";
+////	return uuidAppKey;
+////}
+//
+////SettingsKeeper::SettingsKeeper()
+////{
+////}
+//
+//const int localValue = 17;
+//
+//
+//SettingsKeeper& SettingsKeeper::GetInstance() {
+//	static SettingsKeeper instance;
+//	return instance;
 //}
-
-const int localValue = 17;
-
-
-SettingsKeeper& SettingsKeeper::GetInstance() {
-	static SettingsKeeper instance;
-	return instance;
-}
-
-bool SettingsKeeper::HasAppUUID() {
-	if (localValue == 17) {
-		int xx = 9;
-	}
-	return uuidAppKey == "uuidAppKey";
-}
+//
+//bool SettingsKeeper::HasAppUUID() {
+//	if (localValue == 17) {
+//		int xx = 9;
+//	}
+//	return uuidAppKey == "uuidAppKey";
+//}
--- Console/Static initialization fiasco/SettingsKeeper.h
+++ Console/Static initialization fiasco/SettingsKeeper.h
@@ -1,27 +1,27 @@
-#pragma once
-#include <string>
-
-class SettingsKeeper {
-private:
-	SettingsKeeper() = default;
-
-	~SettingsKeeper() = default;
-public:
-
-	static SettingsKeeper& GetInstance();
-
-	bool HasAppUUID();
-
-	//const std::wstring uuidAppKey = L"uuidAppKey";
-private:
-	// use member constants instead static global variables to ensure that they was initialized when we use this class
-	//const std::string uuidAppKey = "uuidAppKey";
-};
-
-
+//#pragma once
+//#include <string>
+//
 //class SettingsKeeper {
-//	SettingsKeeper() = delete;
-//	~SettingsKeeper() = delete;
+//private:
+//	SettingsKeeper() = default;
+//
+//	~SettingsKeeper() = default;
 //public:
-//	static const std::wstring& uuidAppKey();
+//
+//	static SettingsKeeper& GetInstance();
+//
+//	bool HasAppUUID();
+//
+//	//const std::wstring uuidAppKey = L"uuidAppKey";
+//private:
+//	// use member constants instead static global variables to ensure that they was initialized when we use this class
+//	//const std::string uuidAppKey = "uuidAppKey";
 //};
+//
+//
+////class SettingsKeeper {
+////	SettingsKeeper() = delete;
+////	~SettingsKeeper() = delete;
+////public:
+////	static const std::wstring& uuidAppKey();
+////};
