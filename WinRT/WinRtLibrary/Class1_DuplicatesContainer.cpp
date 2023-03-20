f90284888fb31baf3fa62f7c1711a55e65792b3b
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-03-20 15:18:34 +0200

    Add UWP+ WinRt projects for test c++\cx. Add Class1_DuplicatesContainer.

---
+++ WinRT/WinRtLibrary/Class1_DuplicatesContainer.cpp
@@ -0,0 +1,27 @@
+﻿#include "Class1_DuplicatesContainer.h"
+#include <Windows.h>
+#include <ppltasks.h>
+#include <shellapi.h>
+#include <errhandlingapi.h>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+using namespace WinRtLibrary;
+using namespace Platform;
+
+Class1_DuplicatesContainer::Class1_DuplicatesContainer()
+{		
+	duplicatesCountByFileType = ref new Platform::Collections::Map<Platform::String^, Temp^>();
+	duplicatesCountByFileType->Insert("AAA", ref new Temp{});
+	duplicatesCountByFileType->Insert("BBB", ref new Temp{});
+
+
+	Sleep(2000);
+	Sleep(2000);
+	int xx = 9;
+}
+
+Class1_DuplicatesContainer::~Class1_DuplicatesContainer() {
+	int xxx = 9;
+}
---
+++ WinRT/WinRtLibrary/Class1_DuplicatesContainer.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <map>
+#include <string>
+#include <collection.h>
+
+
+namespace WinRtLibrary
+{
+    private ref class Temp sealed {
+    public:
+        Temp() {
+        }
+
+        virtual ~Temp() {
+            int xxx = 9;
+        }
+    };
+
+    public ref class Class1_DuplicatesContainer sealed
+    {
+    public:
+        Class1_DuplicatesContainer();
+        virtual ~Class1_DuplicatesContainer();
+
+    private:
+        Platform::Collections::Map<Platform::String^, Temp^>^ duplicatesCountByFileType;
+        //std::map<Platform::String^, Temp> duplicatesCountByFileType;
+
+        const std::wstring packageFolder;
+        const std::wstring installedFolder;
+    };
+}
