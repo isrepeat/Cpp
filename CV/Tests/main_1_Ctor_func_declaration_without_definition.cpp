f7e1dc7ec37cbd33452900ebf29a0d61f4170736
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-04-04 17:28:48 +0300

    Add tests

---
+++ CV/Tests/main_1_Ctor_func_declaration_without_definition.cpp
@@ -0,0 +1,10 @@
+class A {
+public:
+	A();
+};
+
+int main() {
+	A a;
+
+	return 0;
+}
---
+++ CV/Tests/main_2_Cropping_objects.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class Parent {
+public:
+	virtual std::string GetName() {
+		return "Parent";
+	};
+};
+
+class Child : public Parent {
+public:
+	std::string GetName() override {
+		return "Child";
+	};
+};
+
+
+
+int main() {
+	Child child;
+	Parent  a = child;
+	Parent& b = child;
+	Parent* c = &child;
+
+	std::cout << a.GetName() << std::endl;
+	std::cout << b.GetName() << std::endl;
+	std::cout << c->GetName() << std::endl;
+	return 0;
+}
