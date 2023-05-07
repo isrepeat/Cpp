130a687826e575fd97b94afcf2ab73fd4f12fecd
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-05-07 23:44:50 +0300

    Add main_25_weak_ptr

--- Console/main_25_try_lock.cpp
+++ Console/main_25_try_lock.cpp
@@ -1,30 +1,30 @@
-#include <Windows.h>
-#include <thread>
-#include <mutex>
-
-void main() {
-	std::mutex mx;
-
-	auto th1 = std::thread([&mx] {
-		Sleep(300);
-		if (auto lock = std::unique_lock{ mx, std::try_to_lock }) {
-			Sleep(2000);
-			printf("thread 1 - owned mx \n");
-		}
-		Sleep(1);
-		});
-
-	auto th2 = std::thread([&mx] {
-		Sleep(200);
-		if (auto lock = std::unique_lock{ mx, std::try_to_lock }) {
-			Sleep(2000);
-			printf("thread 2 - owned mx \n");
-		}
-		Sleep(1);
-		});
-
-	th1.join();
-	th2.join();
-
-	return;
-}
+//#include <Windows.h>
+//#include <thread>
+//#include <mutex>
+//
+//void main() {
+//	std::mutex mx;
+//
+//	auto th1 = std::thread([&mx] {
+//		Sleep(300);
+//		if (auto lock = std::unique_lock{ mx, std::try_to_lock }) {
+//			Sleep(2000);
+//			printf("thread 1 - owned mx \n");
+//		}
+//		Sleep(1);
+//		});
+//
+//	auto th2 = std::thread([&mx] {
+//		Sleep(200);
+//		if (auto lock = std::unique_lock{ mx, std::try_to_lock }) {
+//			Sleep(2000);
+//			printf("thread 2 - owned mx \n");
+//		}
+//		Sleep(1);
+//		});
+//
+//	th1.join();
+//	th2.join();
+//
+//	return;
+//}
---
+++ Console/main_26_weak_ptr.cpp
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include <thread>
+#include <mutex>
+#include <memory>
+#include <functional>
+
+void main() {
+	std::mutex mx;
+	std::weak_ptr<std::function<void()>> callbackWeakPtr;
+	
+	{
+		std::shared_ptr<std::function<void()>> token;
+		{
+			auto sharedPtr = std::make_shared<std::function<void()>>([] {
+				int xx = 9;
+				});
+			callbackWeakPtr = sharedPtr;
+			token = sharedPtr;
+		}
+
+
+		auto locked = callbackWeakPtr.lock();
+		if (locked) {
+			(*locked)();
+		}
+
+		int end = 111;
+	}
+
+	auto locked = callbackWeakPtr.lock();
+	if (locked) {
+		(*locked)();
+	}
+
+	return;
+}
