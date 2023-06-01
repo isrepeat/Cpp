025e22207422966eb1c9f62d4e2c93e4fb738d59
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-06-02 01:27:14 +0300

    Change main_24_future

--- Console/main_24_future.cpp
+++ Console/main_24_future.cpp
@@ -1,56 +1,69 @@
-//#include <Windows.h>
-//#include <functional>
-//#include <thread>
-//#include <Memory>
-//#include <future>
-//
-//std::future<void> routine;
-//std::future<void> routine2;
-//
-//int main() {
-//	{
-//		routine = std::async(std::launch::async, [] {
-//			OutputDebugStringA("111 \n");
-//			Sleep(1000);
-//			OutputDebugStringA("222 \n");
-//			Sleep(1000);
-//			OutputDebugStringA("333 \n");
-//			Sleep(1000);
-//			OutputDebugStringA("444 \n");
-//			Sleep(1000);
-//			OutputDebugStringA("555 \n");
-//			Sleep(1000);
-//			OutputDebugStringA("666 \n");
-//			Sleep(1000);
-//			OutputDebugStringA("777 \n");
-//			Sleep(1000);
-//			OutputDebugStringA("888 \n");
-//			Sleep(1000);
-//			OutputDebugStringA("999 \n");
-//			Sleep(1000);
-//
-//			int xxx = 9;
-//			});
-//	}
-//
-//	Sleep(2200);
-//
-//	{
-//		//routine = std::async(std::launch::async, [] { // previous future destructor called which makes the current thread "Wait" for async task finish
-//		routine2 = std::async(std::launch::async, [] {
-//			OutputDebugStringA("aaa \n");
-//			Sleep(1000);
-//			OutputDebugStringA("bbb \n");
-//			Sleep(1000);
-//			OutputDebugStringA("ccc \n");
-//			Sleep(1000);
-//
-//			int xxx = 9;
-//			});
-//	}
-//
-//	while (1) {
-//		Sleep(100);
-//	}
-//	return 0;
-//}
+#include <Windows.h>
+#include <functional>
+#include <thread>
+#include <Memory>
+#include <future>
+
+std::future<void> routine;
+std::future<void> routine2;
+
+int main() {
+	{
+		routine = std::async(std::launch::async, [] {
+			OutputDebugStringA("111 \n");
+			Sleep(100);
+			//throw 111;
+			OutputDebugStringA("222 \n");
+			Sleep(100);
+			//OutputDebugStringA("333 \n");
+			//Sleep(100);
+			//OutputDebugStringA("444 \n");
+			//Sleep(100);
+			//OutputDebugStringA("555 \n");
+			//Sleep(100);
+			//OutputDebugStringA("666 \n");
+			//Sleep(100);
+			//OutputDebugStringA("777 \n");
+			//Sleep(100);
+			//OutputDebugStringA("888 \n");
+			//Sleep(100);
+			//OutputDebugStringA("999 \n");
+			//Sleep(100);
+
+			int xxx = 9;
+			});
+	}
+
+	Sleep(2200);
+
+	{
+		//routine = std::async(std::launch::async, [] { // previous future destructor called which makes the current thread "Wait" for async task finish
+		routine2 = std::async(std::launch::async, [] {
+			OutputDebugStringA("aaa \n");
+			Sleep(1000);
+			OutputDebugStringA("bbb \n");
+			if (routine.valid()) {
+				if (routine.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+					int xx = 9;
+
+					try {
+						routine.get();
+					}
+					catch (...) {
+						int err = 666;
+					}
+				}
+			}
+			Sleep(100);
+			OutputDebugStringA("ccc \n");
+			Sleep(100);
+
+			int xxx = 9;
+			});
+	}
+
+	while (1) {
+		Sleep(100);
+	}
+	return 0;
+}
