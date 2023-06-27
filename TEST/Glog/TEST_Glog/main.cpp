eed81f492d9ae1ef277f6341b4b67631549e94c7
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-06-27 16:36:01 +0300

    Add TEST_Glog project and "glog" to 3rdParty

---
+++ TEST/Glog/TEST_Glog/main.cpp
@@ -0,0 +1,25 @@
+//#define GLOG_NO_ABBREVIATED_SEVERITIES
+//#include <windows.h>
+
+
+//#include <glog/logging.h>
+//#include <glog/raw_logging.h>
+//
+//#pragma comment(lib, "glog.lib")
+
+int main(int /*argc*/, char** argv) {
+    //FLAGS_alsologtostderr = 1;
+
+    //google::SetLogDestination(google::GLOG_INFO, "D:\\mylog.txt");
+    //google::InitGoogleLogging(argv[0]);
+    ////LOG(INFO) << "Infomration";
+    //RAW_LOG_INFO("Test");
+
+    //RAW_LOG(INFO, "This is INFO");
+    //RAW_LOG(WARNING, "This is WARNING");
+    //RAW_LOG(ERROR, "This is Error");
+    return 0;
+}
+
+// $(SolutionDir)\3rdParty;
+// $(SolutionDir)\3rdParty\glog\lib\$(PlatformShortName);
