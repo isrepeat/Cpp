b543c57451cb2445d989305ae0143ab30da570ec
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-07-13 23:24:22 +0300

    Clean Logger.h/cpp code

--- TEST/Spdlog/TEST_Spdlog/Logger.cpp
+++ TEST/Spdlog/TEST_Spdlog/Logger.cpp
@@ -5,8 +5,6 @@
 #include <spdlog/sinks/msvc_sink.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
-//#include <boost/mp11.hpp>
-
 
 template<typename... Ts>
 struct variant_unwrapper {
@@ -17,20 +15,6 @@ struct variant_unwrapper {
         return std::get<T>(var);
     }
 };
-
-//template<typename T>
-//T& ExpandType(Variant_t variant) {
-//    if ()
-//    return variant_unwrapper{}
-//}
-
-
-template <typename... VariantArgs>
-void PrintVectorImpl(const std::string& format, VariantArgs... variantArgs) {
-    int size = sizeof ...(VariantArgs);
-}
-
-
 
 template<>
 struct fmt::formatter<Variant_t> : formatter<std::string>
@@ -43,80 +27,6 @@ struct fmt::formatter<Variant_t> : formatter<std::string>
             
     }
 };
-
-
-
-//namespace {
-//    struct erased_type : std::vector<std::any> {};
-//}
-//
-//namespace fmt {
-//    template <>
-//    struct formatter<erased_type> : formatter<std::string> {
-//        //template <typename ParseContext>
-//        //constexpr auto parse(ParseContext& ctx) {
-//        //    return ctx.begin();
-//        //}
-//
-//        //template <typename FormatContext>
-//        auto format(const erased_type& v, format_context& ctx) {
-//            auto ctx_itr = ctx.out();
-//            for (auto&& i : v) {
-//                bool casted = false;
-//                using namespace boost::mp11;
-//                mp_for_each<mp_list<int, const char*, double>>([&](auto t) {
-//                    if (!casted) {
-//                        try {
-//                            ctx_itr = format_to(ctx_itr, "{}", std::any_cast<decltype(t)>(i));
-//                            casted = true;
-//                            //ctx_itr++;
-//                        }
-//                        catch (std::bad_any_cast&) {
-//                        }
-//                    }
-//                    });
-//                if (!casted) {
-//                    ctx_itr = format_to(ctx_itr, " UNKNOWN");
-//                }
-//            }
-//            return ctx_itr;
-//        }
-//    };
-//}
-
-//namespace fmt {
-//    template <>
-//    struct formatter<erased_type> {
-//        template <typename ParseContext>
-//        constexpr auto parse(ParseContext& ctx) { 
-//            return ctx.begin(); 
-//        }
-//
-//        template <typename FormatContext>
-//        auto format(const erased_type& v, FormatContext& ctx) {
-//            auto ctx_itr = ctx.begin();
-//            for (auto&& i : v) {
-//                bool b = false;
-//                using namespace boost::mp11;
-//                mp_for_each<mp_list<int, const char*, double>>([&](auto t) {
-//                    if (!b) {
-//                        try {
-//                            format_to(ctx_itr, " {}", std::any_cast<decltype(t)>(i));
-//                            b = true;
-//                            ctx_itr++;
-//                        }
-//                        catch (std::bad_any_cast&) {
-//                        }
-//                    }
-//                    });
-//                if (!b) {
-//                    format_to(ctx_itr++, " UNKNOWN");
-//                }
-//            }
-//            return ctx_itr;
-//        }
-//    };
-//}
 
 
 
@@ -140,7 +50,6 @@ private:
 
 
 
-
 Logger::Logger(std::string logFilePath)
     : impl { new Logger::LoggerImpl(logFilePath) }
 {}
@@ -149,64 +58,10 @@ Logger::~Logger() {
     delete impl;
 }
 
-void Logger::log_impl_any(const std::string& format, std::vector<std::any> vec) {
-    //impl->GetLogger()->info("{0}", erased_type{ std::move(vec) });
-    
-
-    //for (auto&& anyItem : vec) {
-    //    bool casted = false;
-    //    using namespace boost::mp11;
-    //    mp_for_each<mp_list<int, const char*, double>>(
-    //        [&](auto t) {
-    //            if (!casted) {
-    //                try {
-    //                    std::cout << std::any_cast<decltype(t)>(anyItem) << " ";
-    //                    casted = true;
-    //                }
-    //                catch (std::bad_any_cast&) {
-    //                    int xxx = 9;
-    //                }
-    //            }
-    //        });
-
-    //    if (!casted) {
-    //        std::cout << "UNKNOWN ";
-    //    }
-    //}
-    //std::cout << std::endl;
-}
-
-
-
-//template <typename T, typename... Types>
-//T& GetVariantItem(std::variant<std::unique_ptr<Types>...>& variant) {
-//    static const std::unique_ptr<T> emptyPoiner = nullptr;
-//
-//    try {
-//        return std::get<std::unique_ptr<T>>(variant);
-//    }
-//    catch (const std::bad_variant_access&) {
-//        return const_cast<std::unique_ptr<T>&>(emptyPoiner); // remove const
-//    }
-//}
-
-
-
-//void Logger::log_impl_variant(const std::string& format, std::vector<std::variant<int, const char*, double>>&& vec) {
-//    for (auto&& variantItem : vec) {
-//    }
-//}
-
-//template <typename... Args>
-//void Logger::info(const char* fmt, Args &... args) {
-//    _imp->info(fmt, args...);
-//}
-
 template <typename... VariantArgs>
 void Logger::log_variant_impl(const std::string& format, VariantArgs... variants) {
     this->impl->GetLogger()->info(format, variants...);
 }
-
 
 template void Logger::log_variant_impl(const std::string&, Variant_t);
 template void Logger::log_variant_impl(const std::string&, Variant_t, Variant_t);
--- TEST/Spdlog/TEST_Spdlog/Logger.h
+++ TEST/Spdlog/TEST_Spdlog/Logger.h
@@ -13,20 +13,12 @@ public:
     Logger(std::string logFilePath);
     ~Logger();
     
-
-    template <class... Args>
-    void logAny(const std::string& format, Args&&... args) {
-        log_impl_any(format, { std::any(std::forward<Args>(args))... });
-    }
-
     template <class... Args>
     void logVariant(const std::string& format, Args... args) {
         log_variant_impl(format, std::forward<Variant_t>(args)...);
     }
 
 private:
-    void log_impl_any(const std::string& format, std::vector<std::any> vec);
-
     template <typename... VariantArgs>
     void log_variant_impl(const std::string& format, VariantArgs... variants);
 
