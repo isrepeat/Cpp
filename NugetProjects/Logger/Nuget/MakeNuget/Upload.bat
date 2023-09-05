3a870366fd72df09c6d671e6762460f10454b46a
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-09-05 18:08:03 +0300

    Add PublicIncludeDirectories to Logger.targets

--- NugetProjects/Logger/Nuget/MakeNuget/Upload.bat
+++ NugetProjects/Logger/Nuget/MakeNuget/Upload.bat
@@ -8,7 +8,7 @@ SET /P AREYOUSURE=Upload nuget (Y/[N])?
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 echo upload nuget ...
-nuget push -Source http://nuget.dct.ua/v3/index.json Output\Logger.1.1.0.nupkg
+nuget push -Source http://nuget.dct.ua/v3/index.json Output\Logger.1.1.2.nupkg
 
 :END
 endlocal
