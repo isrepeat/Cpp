1fdc9d65b1ae6bc4b6c6acbd859e5534cdb01fe6
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-09-05 16:13:51 +0300

    Write Upload.bat for nugets

---
+++ NugetProjects/Logger/Nuget/MakeNuget/Upload.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set PATH=%PATH%;%~dp0..\..\..\..\Tools\
+
+setlocal
+:PROMPT
+SET /P AREYOUSURE=Upload nuget (Y/[N])?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+echo upload nuget ...
+nuget push -Source http://nuget.dct.ua/v3/index.json Output\Logger.1.1.0.nupkg
+
+:END
+endlocal
+
+pause
