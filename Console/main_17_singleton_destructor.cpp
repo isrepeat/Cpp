89c18cb0b0d195b51400f386f6ed29cebc40af91
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-11-06 21:15:22 +0200

    Change main_40_filesystem. Add FilesCollection.

--- Console/main_17_singleton_destructor.cpp
+++ Console/main_17_singleton_destructor.cpp
@@ -1,244 +1,244 @@
-#include <Windows.h>
-#include <iostream>
-#include <cassert>
-#include <string>
-#include <memory>
-#include <mutex>
-
-
-template <typename TClass, typename T = void*>
-class _Singleton {
-public:
-    // Call this in constructors of singletons classes (to ensure that TClass destroy after all of them)
-    static void InitSingleton() {
-        GetInstance();
-    }
-
-    // Work with public Ctor without args
-    static TClass& GetInstance() {
-        static TClass instance;
-        return instance;
-    }
-
-private:
-    friend TClass;
-    _Singleton() = default;
-    ~_Singleton() = default;
-
-private:
-    T m_instance;
-};
-
-
-template <typename C>
-class Singleton : public _Singleton<Singleton<C>, std::unique_ptr<C>> {
-private:
-    using _MyBase = _Singleton<Singleton<C>, std::unique_ptr<C>>;
-
-public:
-    using Instance_t = C&;
-
-    Singleton() = default;
-    ~Singleton() {
-        int xx = 9;
-    };
-
-    template <typename ...Args>
-    static Instance_t CreateInstance(Args... args) {
-        auto& _this = _MyBase::GetInstance();
-        std::unique_lock lk{ _this.mx };
-        if (_this.m_instance == nullptr) {
-            _this.m_instance = std::make_unique<C>(args...);
-        }
-        return *_this.m_instance;
-    }
-
-    static Instance_t GetInstance() {
-        assert(_MyBase::GetInstance().m_instance);
-        return *_MyBase::GetInstance().m_instance;
-    }
-
-private:
-    std::mutex mx;
-};
-
-
-
-template <typename C>
-class SingletonShared : public _Singleton<SingletonShared<C>, std::shared_ptr<C>> {
-private:
-    using _MyBase = _Singleton<SingletonShared<C>, std::shared_ptr<C>>;
-
-public:
-    using Instance_t = std::shared_ptr<C>;
-
-    SingletonShared() = default;
-    ~SingletonShared() {
-        int xx = 9;
-    };
-
-    template <typename ...Args>
-    static Instance_t CreateInstance(Args... args) {
-        auto& _this = _MyBase::GetInstance();
-        std::unique_lock lk{ _this.mx };
-        if (_this.m_instance == nullptr) {
-            _this.m_instance = std::make_shared<C>(args...);
-        }
-        return _this.m_instance;
-    }
-
-    static Instance_t GetInstance() {
-        assert(_MyBase::GetInstance().m_instance);
-        return _MyBase::GetInstance().m_instance;
-    }
-
-private:
-    std::mutex mx;
-};
-
-
-template <typename C>
-class SingletonUnscoped : public _Singleton<SingletonUnscoped<C>, C*> {
-private:
-    using _MyBase = _Singleton<SingletonUnscoped<C>, C*>;
-
-public:
-    using Instance_t = C*;
-
-    SingletonUnscoped() = default;
-    ~SingletonUnscoped() {
-        int xx = 9;
-    };
-
-    template <typename ...Args>
-    static Instance_t CreateInstance(Args... args) {
-        auto& _this = _MyBase::GetInstance();
-        std::unique_lock lk{ _this.mx };
-        if (_this.m_instance == nullptr) {
-            _this.m_instance = new C(args...);
-        }
-        return _this.m_instance;
-    }
-
-    static Instance_t GetInstance() {
-        assert(_MyBase::GetInstance().m_instance);
-        return _MyBase::GetInstance().m_instance;
-    }
-
-private:
-    std::mutex mx;
-};
-
-
-
-class Temp {
-public:
-    Temp(int param1, float param2) 
-        : param1{ param1 } 
-        , param2{ param2 }
-    {
-        int xxx = 9;
-    }
-    ~Temp() {
-        int xxx = 9;
-    }
-
-    float GetParam2() {
-        return param2;
-    }
-
-private:
-    int param1;
-    float param2;
-};
-
-
-class TempS : public _Singleton<class TempS> {
-private:
-    using _MyBase = _Singleton<TempS>;
-    friend _MyBase;
-
-    TempS()
-        : param1{ 123 }
-        , param2{ 9.11f }
-    {
-        int xxx = 9;
-    }
-
-public:
-    ~TempS() {
-        int xxx = 9;
-    }
-
-    static TempS& GetInstance() {
-        return _MyBase::GetInstance();
-    }
-
-    float GetParam2() {
-        return param2;
-    }
-
-private:
-    std::mutex ms;
-    int param1;
-    float param2;
-};
-
-
-
-//using TempSingleton = Singleton<Temp>;
-using TempSingleton = SingletonShared<Temp>;
-//using TempSingleton = SingletonUnscoped<Temp>;
-
-
-
-
-class Application {
-    Application() {
-        TempS::InitSingleton();
-        TempSingleton::InitSingleton();
-    }
-public:
-    ~Application() {
-        float xx = TempSingleton::GetInstance()->GetParam2();
-        float yy = TempS::GetInstance().GetParam2();
-        int zz = 9;
-    }
-
-    static Application& GetInstance() {
-        static Application instance;
-        return instance;
-    }
-};
-
-
-class Application_2 {
-    Application_2() {
-        TempS::InitSingleton();
-        TempSingleton::InitSingleton();
-    }
-public:
-    ~Application_2() {
-        float xx = TempSingleton::GetInstance()->GetParam2();
-        float yy = TempS::GetInstance().GetParam2();
-        int zz = 9;
-    }
-
-    static Application_2& GetInstance() {
-        static Application_2 instance;
-        return instance;
-    }
-};
-
-
-int main() {
-    Application::GetInstance();
-    Application_2::GetInstance();
-
-    TempSingleton::Instance_t a = TempSingleton::CreateInstance(111, 3.14f);
-    TempSingleton::Instance_t b = TempSingleton::GetInstance();
-
-    TempS& c = TempS::GetInstance();
-
-	return 0;
-}
+//#include <Windows.h>
+//#include <iostream>
+//#include <cassert>
+//#include <string>
+//#include <memory>
+//#include <mutex>
+//
+//
+//template <typename TClass, typename T = void*>
+//class _Singleton {
+//public:
+//    // Call this in constructors of singletons classes (to ensure that TClass destroy after all of them)
+//    static void InitSingleton() {
+//        GetInstance();
+//    }
+//
+//    // Work with public Ctor without args
+//    static TClass& GetInstance() {
+//        static TClass instance;
+//        return instance;
+//    }
+//
+//private:
+//    friend TClass;
+//    _Singleton() = default;
+//    ~_Singleton() = default;
+//
+//private:
+//    T m_instance;
+//};
+//
+//
+//template <typename C>
+//class Singleton : public _Singleton<Singleton<C>, std::unique_ptr<C>> {
+//private:
+//    using _MyBase = _Singleton<Singleton<C>, std::unique_ptr<C>>;
+//
+//public:
+//    using Instance_t = C&;
+//
+//    Singleton() = default;
+//    ~Singleton() {
+//        int xx = 9;
+//    };
+//
+//    template <typename ...Args>
+//    static Instance_t CreateInstance(Args... args) {
+//        auto& _this = _MyBase::GetInstance();
+//        std::unique_lock lk{ _this.mx };
+//        if (_this.m_instance == nullptr) {
+//            _this.m_instance = std::make_unique<C>(args...);
+//        }
+//        return *_this.m_instance;
+//    }
+//
+//    static Instance_t GetInstance() {
+//        assert(_MyBase::GetInstance().m_instance);
+//        return *_MyBase::GetInstance().m_instance;
+//    }
+//
+//private:
+//    std::mutex mx;
+//};
+//
+//
+//
+//template <typename C>
+//class SingletonShared : public _Singleton<SingletonShared<C>, std::shared_ptr<C>> {
+//private:
+//    using _MyBase = _Singleton<SingletonShared<C>, std::shared_ptr<C>>;
+//
+//public:
+//    using Instance_t = std::shared_ptr<C>;
+//
+//    SingletonShared() = default;
+//    ~SingletonShared() {
+//        int xx = 9;
+//    };
+//
+//    template <typename ...Args>
+//    static Instance_t CreateInstance(Args... args) {
+//        auto& _this = _MyBase::GetInstance();
+//        std::unique_lock lk{ _this.mx };
+//        if (_this.m_instance == nullptr) {
+//            _this.m_instance = std::make_shared<C>(args...);
+//        }
+//        return _this.m_instance;
+//    }
+//
+//    static Instance_t GetInstance() {
+//        assert(_MyBase::GetInstance().m_instance);
+//        return _MyBase::GetInstance().m_instance;
+//    }
+//
+//private:
+//    std::mutex mx;
+//};
+//
+//
+//template <typename C>
+//class SingletonUnscoped : public _Singleton<SingletonUnscoped<C>, C*> {
+//private:
+//    using _MyBase = _Singleton<SingletonUnscoped<C>, C*>;
+//
+//public:
+//    using Instance_t = C*;
+//
+//    SingletonUnscoped() = default;
+//    ~SingletonUnscoped() {
+//        int xx = 9;
+//    };
+//
+//    template <typename ...Args>
+//    static Instance_t CreateInstance(Args... args) {
+//        auto& _this = _MyBase::GetInstance();
+//        std::unique_lock lk{ _this.mx };
+//        if (_this.m_instance == nullptr) {
+//            _this.m_instance = new C(args...);
+//        }
+//        return _this.m_instance;
+//    }
+//
+//    static Instance_t GetInstance() {
+//        assert(_MyBase::GetInstance().m_instance);
+//        return _MyBase::GetInstance().m_instance;
+//    }
+//
+//private:
+//    std::mutex mx;
+//};
+//
+//
+//
+//class Temp {
+//public:
+//    Temp(int param1, float param2) 
+//        : param1{ param1 } 
+//        , param2{ param2 }
+//    {
+//        int xxx = 9;
+//    }
+//    ~Temp() {
+//        int xxx = 9;
+//    }
+//
+//    float GetParam2() {
+//        return param2;
+//    }
+//
+//private:
+//    int param1;
+//    float param2;
+//};
+//
+//
+//class TempS : public _Singleton<class TempS> {
+//private:
+//    using _MyBase = _Singleton<TempS>;
+//    friend _MyBase;
+//
+//    TempS()
+//        : param1{ 123 }
+//        , param2{ 9.11f }
+//    {
+//        int xxx = 9;
+//    }
+//
+//public:
+//    ~TempS() {
+//        int xxx = 9;
+//    }
+//
+//    static TempS& GetInstance() {
+//        return _MyBase::GetInstance();
+//    }
+//
+//    float GetParam2() {
+//        return param2;
+//    }
+//
+//private:
+//    std::mutex ms;
+//    int param1;
+//    float param2;
+//};
+//
+//
+//
+////using TempSingleton = Singleton<Temp>;
+//using TempSingleton = SingletonShared<Temp>;
+////using TempSingleton = SingletonUnscoped<Temp>;
+//
+//
+//
+//
+//class Application {
+//    Application() {
+//        TempS::InitSingleton();
+//        TempSingleton::InitSingleton();
+//    }
+//public:
+//    ~Application() {
+//        float xx = TempSingleton::GetInstance()->GetParam2();
+//        float yy = TempS::GetInstance().GetParam2();
+//        int zz = 9;
+//    }
+//
+//    static Application& GetInstance() {
+//        static Application instance;
+//        return instance;
+//    }
+//};
+//
+//
+//class Application_2 {
+//    Application_2() {
+//        TempS::InitSingleton();
+//        TempSingleton::InitSingleton();
+//    }
+//public:
+//    ~Application_2() {
+//        float xx = TempSingleton::GetInstance()->GetParam2();
+//        float yy = TempS::GetInstance().GetParam2();
+//        int zz = 9;
+//    }
+//
+//    static Application_2& GetInstance() {
+//        static Application_2 instance;
+//        return instance;
+//    }
+//};
+//
+//
+//int main() {
+//    Application::GetInstance();
+//    Application_2::GetInstance();
+//
+//    TempSingleton::Instance_t a = TempSingleton::CreateInstance(111, 3.14f);
+//    TempSingleton::Instance_t b = TempSingleton::GetInstance();
+//
+//    TempS& c = TempS::GetInstance();
+//
+//	return 0;
+//}
--- Console/main_40_filesystem.cpp
+++ Console/main_40_filesystem.cpp
@@ -1,37 +1,108 @@
-//#include <Windows.h>
-//#include <Helpers/Helpers.h>
-//#include <filesystem>
-//#include <iostream>
-//#include <string>
-//
-//
-//const std::wstring rootDir = H::ExePathW() + L"\\TestFilesystem\\";
-//
-//void TestCopyDirectoryContentToAnotherDirectory() {
-//    std::wstring fromDir = rootDir + L"Logs";
-//    std::wstring toDir = rootDir +  L"CrashReport";
-//
-//    if (!std::filesystem::exists(fromDir)) {
-//        throw std::exception("dir not exist");
-//    }
-//
-//    if (!std::filesystem::exists(toDir)) {
-//        std::filesystem::create_directory(toDir);
-//    }
-//
-//    std::filesystem::copy(fromDir, toDir, std::filesystem::copy_options::overwrite_existing | std::filesystem::copy_options::recursive);
-//    return;
-//}
-//
-//
-//void TestPathToFile() {
-//    std::wstring filename = L"D:\\WORK\\TEST\\AAA\\BBB\\foo.txt";
-//    std::wstring folder = std::filesystem::path(filename).parent_path();
-//    return;
-//}
-//
-//void main() {
-//    //TestCopyDirectoryContentToAnotherDirectory();
-//    TestPathToFile();
-//    return;
-//}
+#include <Windows.h>
+#include <Helpers/Helpers.h>
+#include <filesystem>
+#include <iostream>
+#include <string>
+
+
+const std::wstring rootDir = H::ExePathW() + L"\\TestFilesystem\\";
+
+void TestCopyDirectoryContentToAnotherDirectory() {
+    std::wstring fromDir = rootDir + L"Logs";
+    std::wstring toDir = rootDir +  L"CrashReport";
+
+    if (!std::filesystem::exists(fromDir)) {
+        throw std::exception("dir not exist");
+    }
+
+    if (!std::filesystem::exists(toDir)) {
+        std::filesystem::create_directory(toDir);
+    }
+
+    std::filesystem::copy(fromDir, toDir, std::filesystem::copy_options::overwrite_existing | std::filesystem::copy_options::recursive);
+    return;
+}
+
+
+void TestPathToFile() {
+    std::wstring filename = L"D:\\WORK\\TEST\\AAA\\BBB\\foo.txt";
+    std::wstring folder = std::filesystem::path(filename).parent_path();
+    return;
+}
+
+
+
+struct FilesCollection {
+    enum class Format {
+        Default,
+        RelativePath,
+    };
+
+    FilesCollection(std::vector<std::filesystem::path> filePaths, Format format = Format::Default) {
+        for (auto& item : filePaths) {
+            if (std::filesystem::is_regular_file(item)) {
+                files.push_back(item);
+                totalSize += std::filesystem::file_size(item);
+            }
+            else if (std::filesystem::is_directory(item)) {
+                dirs.push_back(item);
+
+                for (const auto& dirEntry : std::filesystem::recursive_directory_iterator(item)) {
+                    if (dirEntry.is_regular_file()) {
+                        files.push_back(dirEntry.path());
+                        totalSize += std::filesystem::file_size(dirEntry);
+                    }
+                    else if (dirEntry.is_directory()) {
+                        dirs.push_back(dirEntry);
+                    }
+                }
+            }
+        }
+
+        if (format == Format::RelativePath) { // remove root path
+            for (auto& file : files) {
+                file = file.relative_path() / file.filename();
+            }
+            for (auto& dir : dirs) {
+                dir = dir.relative_path();
+            }
+        }
+    };
+
+    const std::vector<std::filesystem::path>& GetFiles() const {
+        return files;
+    }
+
+    const std::vector<std::filesystem::path>& GetDirs() const {
+        return dirs;
+    }
+
+    uint64_t GetSize() const {
+        return totalSize;
+    }
+
+private:
+    std::vector<std::filesystem::path> files;
+    std::vector<std::filesystem::path> dirs;
+    uint64_t totalSize = 0;
+};
+
+
+void TestFilesCollection() {    
+    std::vector<std::filesystem::path> fileList = {
+        L"D:\\DiskDir_2",
+        L"D:\\black.png",
+    };
+
+    auto filesCollection_1 = FilesCollection(fileList);
+    auto filesCollection_2 = FilesCollection(fileList, FilesCollection::Format::RelativePath);
+
+    return;
+}
+
+void main() {
+    //TestCopyDirectoryContentToAnotherDirectory();
+    //TestPathToFile();
+    TestFilesCollection();
+    return;
+}
