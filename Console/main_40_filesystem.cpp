83a3d6e8ea0ef57b418a536a9899baf03c3e4dcc
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-11-08 18:11:03 +0200

    Change main_40_filesystem. Add TestVolumeInfo() + PathItem

--- Console/main_40_filesystem.cpp
+++ Console/main_40_filesystem.cpp
@@ -2,6 +2,7 @@
 #include <Helpers/Helpers.h>
 #include <filesystem>
 #include <iostream>
+#include <cassert>
 #include <string>
 
 
@@ -37,42 +38,126 @@ void TestPathToFile() {
 }
 
 
+/* ------------------ */
+/*   TestVolumeInfo   */
+/* ------------------ */
+void TestVolumeInfo() {
+    wchar_t szVolumeName[100] = L"";
+    wchar_t szFileSystemName[10] = L"";
+    DWORD dwSerialNumber = 0;
+    DWORD dwMaxFileNameLength = 0;
+    DWORD dwFileSystemFlags = 0;
+
+    if (!GetVolumeInformationW(L"D:\\", 
+        szVolumeName, 
+        sizeof(szVolumeName),
+        &dwSerialNumber,
+        &dwMaxFileNameLength,
+        &dwFileSystemFlags,
+        szFileSystemName, 
+        sizeof(szFileSystemName)))
+    {
+        int fail = 1;
+    }
+
+    struct VolumeInfo {
+        std::wstring volumeName;
+        std::wstring fileSystemName;
+    };
+
+    VolumeInfo volumeInfo;
+    volumeInfo.volumeName = szVolumeName;
+    volumeInfo.fileSystemName = szFileSystemName;
+
+    return;
+}
+
+
 
 
 /* ----------------------- */
 /*   TestFilesCollection   */
 /* ----------------------- */
-#define define_has_member(member_name)                                         \
-    template <typename T>                                                      \
-    class has_member_##member_name                                             \
-    {                                                                          \
-        typedef char yes_type;                                                 \
-        typedef long no_type;                                                  \
-        template <typename U> static yes_type test(decltype(&U::member_name)); \
-        template <typename U> static no_type  test(...);                       \
-    public:                                                                    \
-        static constexpr bool value = sizeof(test<T>(0)) == sizeof(yes_type);  \
+struct PathItem {
+    enum class Type {
+        File,
+        Directory,
+        RecursiveEntry,
+    };
+
+    Type ExpandType() const {
+        if (type == PathItem::Type::RecursiveEntry) {
+            assert(!recursiveItem.empty() && "--> recursiveItem is empty!");
+
+            if (std::filesystem::is_regular_file(recursiveItem)) {
+                return Type::File;
+            }
+            else if (std::filesystem::is_directory(recursiveItem)) {
+                return Type::Directory;
+            }
+        }
+        return type;
     }
 
-#define has_member(class_, member_name)  has_member_##member_name<class_>::value
+    Type type;
+    std::filesystem::path mainItem;
+    std::filesystem::path recursiveItem;
+};
 
-struct IFilesCollection {
-    //enum class Type {
-    //    RegularFile,
-    //    Directory,
-    //};
-
+class IFilesCollection {
+public:
     virtual ~IFilesCollection() = default;
 
 protected:
     friend class FilesObserver;
     virtual void Initialize() = 0;
-    virtual void HandlePathItem(std::filesystem::path item) = 0;
+    virtual void HandlePathItem(const PathItem& pathItem) = 0;
     virtual void Complete() = 0;
 };
 
 
-struct FilesCollection : public IFilesCollection {
+class FilesObserver {
+private:
+    FilesObserver() = delete;
+    ~FilesObserver() = delete;
+
+public:
+    template <typename TCollection>
+    static void GetFilesCollection(const std::vector<std::filesystem::path>& filePaths, TCollection& filesCollection) {
+        IFilesCollection& filesCollectionInterface = filesCollection;
+        filesCollectionInterface.Initialize();
+
+        for (auto& item : filePaths) {
+            if (std::filesystem::is_regular_file(item)) {
+                filesCollectionInterface.HandlePathItem(PathItem{ PathItem::Type::File, std::move(item) });
+            }
+            else if (std::filesystem::is_directory(item)) {
+                PathItem pathItem{ PathItem::Type::Directory, std::move(item) };
+                filesCollectionInterface.HandlePathItem(pathItem);
+
+                for (const auto& dirEntry : std::filesystem::recursive_directory_iterator(item)) {
+                    pathItem.type = PathItem::Type::RecursiveEntry;
+                    pathItem.recursiveItem = dirEntry.path();
+                    filesCollectionInterface.HandlePathItem(pathItem);
+                }
+            }
+        }
+        filesCollectionInterface.Complete();
+    };
+
+
+    template <typename TCollection = FilesCollection>
+    static TCollection GetFilesCollection(const std::vector<std::filesystem::path>& filePaths) {
+        TCollection filesCollection; // TCollection must have default Ctor
+        GetFilesCollection<TCollection>(filePaths, filesCollection);
+        return filesCollection;
+    };
+};
+
+
+
+class FilesCollection : public IFilesCollection {
+public:
     FilesCollection() = default;
 
     void ReplaceRootPath(std::wstring newRootPath) {
@@ -80,7 +165,7 @@ struct FilesCollection : public IFilesCollection {
             dir = newRootPath / dir.relative_path();
         }
         for (auto& file : files) {
-            file = newRootPath / file.relative_path() / file.filename();
+            file = newRootPath / file.relative_path();
         }
     }
 
@@ -103,13 +188,15 @@ private:
         totalSize = 0;
     }
 
-    void HandlePathItem(std::filesystem::path item) override {
-        if (std::filesystem::is_regular_file(item)) {
-            files.push_back(item);
-            totalSize += std::filesystem::file_size(item);
-        }
-        else if (std::filesystem::is_directory(item)) {
-            dirs.push_back(item);
+    void HandlePathItem(const PathItem& pathItem) override {
+        switch (pathItem.ExpandType()) {
+        case PathItem::Type::File:
+            files.push_back(pathItem.mainItem);
+            totalSize += std::filesystem::file_size(pathItem.mainItem);
+            break;
+        case PathItem::Type::Directory:
+            dirs.push_back(pathItem.mainItem);
+            break;
         }
     }
 
@@ -117,32 +204,52 @@ private:
     }
 
 private:
+    uint64_t totalSize = 0;
     std::vector<std::filesystem::path> dirs;
     std::vector<std::filesystem::path> files;
-    uint64_t totalSize = 0;
 };
 
 
 
-struct DiscItem {
+
+
+struct DiscFileItem {
     std::filesystem::path localPath;
     std::filesystem::path discPath;
 };
 
-struct DiscFilesCollection : public IFilesCollection {
-    DiscFilesCollection(wchar_t discLabel) 
-        : diskRootPath{ discLabel + std::wstring(L":\\") }
-    {
+class DiscFilesCollection : public IFilesCollection {
+public:
+    enum class Format {
+        Default,
+        RelativeDiscPath,
+    };
+
+    DiscFilesCollection(wchar_t discLetter, Format format = Format::Default)
+        : totalSize{ 0 }
+        , format{ format }
+        , discRootPath{ discLetter + std::wstring(L":\\") }
+    {}
+    DiscFilesCollection(DiscFilesCollection&) = default;
+
+    // After copying, internally calls Complete()
+    DiscFilesCollection& operator=(const DiscFilesCollection& other) {
+        if (this != &other) {
+            dirs = other.dirs;
+            files = other.files;
+            totalSize = other.totalSize;
+            discRootPath = other.discRootPath;
+            Complete();
+        }
+        return *this;
     }
 
-    const std::vector<DiscItem>& GetDirs() const {
+    const std::vector<DiscFileItem>& GetDirs() const {
         return dirs;
     }
-
-    const std::vector<DiscItem>& GetFiles() const {
+    const std::vector<DiscFileItem>& GetFiles() const {
         return files;
     }
-
     const uint64_t GetSize() const {
         return totalSize;
     }
@@ -154,77 +261,75 @@ private:
         totalSize = 0;
     }
 
-    void HandlePathItem(std::filesystem::path item) override {
-        if (std::filesystem::is_regular_file(item)) {
-            files.push_back(DiscItem{ item, diskRootPath / item.relative_path() });
-            totalSize += std::filesystem::file_size(item);
+    void HandlePathItem(const PathItem& pathItem) override {
+        DiscFileItem discFileItem;
+
+        switch (pathItem.type) {
+        case PathItem::Type::File:
+        case PathItem::Type::Directory:
+            discFileItem = { pathItem.mainItem, discRootPath / pathItem.mainItem.filename() };
+            break;
+        case PathItem::Type::RecursiveEntry: {
+            // Cut mainItem path part from recursiveItem (recursiveItem is a child item of mainItem)
+            auto relativePathToMainItem = pathItem.recursiveItem.wstring().substr(pathItem.mainItem.wstring().size() + 1); // +1 - skip slash
+            discFileItem = { pathItem.recursiveItem, discRootPath / pathItem.mainItem.filename() / relativePathToMainItem };
+            break;
         }
-        else if (std::filesystem::is_directory(item)) {
-            dirs.push_back(DiscItem{ item, diskRootPath / item.relative_path() });
+        }
+
+        switch (pathItem.ExpandType()) {
+        case PathItem::Type::File:
+            files.push_back(std::move(discFileItem));
+            totalSize += std::filesystem::file_size(pathItem.mainItem);
+            break;
+        case PathItem::Type::Directory:
+            dirs.push_back(std::move(discFileItem));
+            break;
         }
     }
 
     void Complete() override {
+        if (format == Format::RelativeDiscPath) { // remove root path
+            for (auto& dir : dirs) {
+                dir.discPath = dir.discPath.relative_path();
+            }
+            for (auto& file : files) {
+                file.discPath = file.discPath.relative_path();
+            }
+        }
     }
 
 private:
-    std::wstring diskRootPath;
-    std::vector<DiscItem> dirs;
-    std::vector<DiscItem> files;
-    uint64_t totalSize = 0;
+    uint64_t totalSize;
+    const Format format;
+    std::filesystem::path discRootPath;
+    std::vector<DiscFileItem> dirs;
+    std::vector<DiscFileItem> files;
 };
 
 
-class FilesObserver {
-    FilesObserver() = delete;
-
-public:
-    template <typename TCollection>
-    static void GetFilesCollection(const std::vector<std::filesystem::path>& filePaths, TCollection& filesCollection) {
-        IFilesCollection& filesCollectionInterface = filesCollection; // TODO: use type traits on base class to validate static_cast at compile time
-
-        filesCollectionInterface.Initialize();
-
-        for (auto& item : filePaths) {
-            if (std::filesystem::is_regular_file(item)) {
-                filesCollectionInterface.HandlePathItem(item);
-            }
-            else if (std::filesystem::is_directory(item)) {
-                filesCollectionInterface.HandlePathItem(item);
-
-                for (const auto& dirEntry : std::filesystem::recursive_directory_iterator(item)) {
-                    filesCollectionInterface.HandlePathItem(dirEntry.path());
-                }
-            }
-        }
-        filesCollectionInterface.Complete();
-    };
-
-    template <typename TCollection = FilesCollection>
-    static TCollection GetFilesCollection(const std::vector<std::filesystem::path>& filePaths) {
-        TCollection filesCollection; // TCollection must have default Ctor
-        GetFilesCollection<TCollection>(filePaths, filesCollection);
-        return filesCollection;
-    };
-};
 
 
 void TestFilesCollection() {    
+    //std::filesystem::path tmpPath = L"C:\\TORRENT_TMP\\Hello\\";
+    std::filesystem::path tmpPath = L"\\TORRENT_TMP\\Hello";
+    auto rootPath = tmpPath.root_path();
+    auto relPath = tmpPath.relative_path();
+    auto filename = tmpPath.filename();
+
+
+    //auto xx = rootPath.re
+
     std::vector<std::filesystem::path> fileList = {
         L"D:\\black.png",
         L"D:\\DiskDir_2",
         L"C:\\TORRENT_TMP\\Hello",
     };
 
-    auto rel_path = fileList[0].relative_path();
-    auto root_path = fileList[0].root_path();
-
     auto filesCollection_implicit = FilesObserver::GetFilesCollection(fileList);
 
     FilesCollection filesCollection_explicit;
     FilesObserver::GetFilesCollection(fileList, filesCollection_explicit);
-
-    //auto discFilesCollection_explicit = FilesObserver::GetFilesCollection<DiscFilesCollection>(fileList);
 
     DiscFilesCollection discFilesCollection_explicit('H');
     FilesObserver::GetFilesCollection(fileList, discFilesCollection_explicit);
@@ -232,9 +337,12 @@ void TestFilesCollection() {
     return;
 }
 
+
 void main() {
     //TestCopyDirectoryContentToAnotherDirectory();
     //TestPathToFile();
-    TestFilesCollection();
+    TestVolumeInfo();
+    //TestFilesCollection();
+
     return;
 }
