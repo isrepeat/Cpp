081026a58db9c5e81168f5adf43d017a8f5957c0
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-12-15 13:14:41 +0200

    Add counter

--- CrossPlatform/HelloWorld/src/main.cpp
+++ CrossPlatform/HelloWorld/src/main.cpp
@@ -1,9 +1,15 @@
 #include "PlatformProvider.h"
 #include <functional>
 #include <iostream>
+#include <thread>
 
 int main() {
 	PlatformProvider platformProvider;
 	std::cout << "Hello " << platformProvider.GetOsName() << "!" << std::endl;
+	
+	for (int i = 1; i <= 5; i++) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(1'000));
+		std::cout << i << std::endl;
+	}
 	return 0;
 }
