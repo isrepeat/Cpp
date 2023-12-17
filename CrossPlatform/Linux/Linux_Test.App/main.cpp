2a671c66980abe32be453098671c34f28461a6dd
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2023-12-18 00:18:47 +0200

    Add CMakeLists for Linux_Test.App and root.

--- CrossPlatform/Linux/Linux_Test.App/main.cpp
+++ CrossPlatform/Linux/Linux_Test.App/main.cpp
@@ -3,14 +3,14 @@
 #include <thread>
 
 //#include "../Linux_Test.Static/ClassFromStaticLib.h"
-#include "../Linux_Test.Dynamic/ClassFromDynamicLib.h"
+//#include "../Linux_Test.Dynamic/ClassFromDynamicLib.h"
 
-int main() {
+int main() { 
 	std::cout << "Hello from Linux_Test.App" << std::endl;
 
 	//ClassFromStaticLib libraryClass;
-	ClassFromDynamicLib libraryClass;
-	std::cout << "Library print = \"" << libraryClass.Print() << "\"" << std::endl;
+	//ClassFromDynamicLib libraryClass;
+	//std::cout << "Library print = \"" << libraryClass.Print() << "\"" << std::endl;
 
 	for (int i = 1; i <= 3; i++) {
 		std::this_thread::sleep_for(std::chrono::milliseconds(1'000));
