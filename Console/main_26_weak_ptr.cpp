524fd1eaf7fd9670f68443c71ec5a02c524fbd2d
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-01-19 13:49:55 +0200

    Change main_26_weak_ptr. Add MsQuicInitializer.

--- Console/main_26_weak_ptr.cpp
+++ Console/main_26_weak_ptr.cpp
@@ -1,3 +1,5 @@
+#include <Helpers/Logger.h>
+#include <Helpers/Time.h>
 #include <Windows.h>
 #include <functional>
 #include <memory>
@@ -96,8 +98,136 @@ void TestWeakAndDestuctorShared() {
 	if (weakPtr.expired()) {
 		printf("weakPtr expired \n");
 	}
+
 	Sleep(100);
+	return;
 }
+
+
+
+class MsQuicInitializer {
+public:
+	MsQuicInitializer(const MsQuicInitializer&) = delete;
+	MsQuicInitializer(MsQuicInitializer&&) = delete;
+
+	static MsQuicInitializer& GetInstance() {
+		static auto instance = MsQuicInitializer{};
+		return instance;
+	}
+
+	struct Context {
+		void* context = nullptr;
+		std::weak_ptr<int> token;
+		std::function<void()> shutdownConnectionCallback = nullptr;
+	};
+	Context* AddContextWithToken(void* context, std::shared_ptr<int> token) {
+		LOG_FUNCTION_ENTER("AddContextWithToken(context, token)");
+		listContexts.push_back(Context{
+			context,
+			token,
+			});
+
+		auto& pushedItem = listContexts.back();
+		LOG_DEBUG_D("pushedItem = {}", static_cast<void*>(&pushedItem));
+
+		pushedItem.shutdownConnectionCallback = [this, currElemIterator = --listContexts.end()]{ // save iterator on current(last) element to erase it later
+			// NOTE: Alternatively you can use a timer to remove old expired context if was problem with syncroniously deletion form QUIC connection callback.
+			//H::Timer::Once(10'000ms, [this, currElemIterator] {
+			LOG_FUNCTION_ENTER("shutdownConnectionCallback()");
+			LOG_DEBUG_D("deletionItem = {}", static_cast<void*>(&(*currElemIterator)));
+			if (currElemIterator->token.expired()) {
+				listContexts.erase(currElemIterator); // remove expired context from list
+			}
+		//});
+		};
+		return &pushedItem;
+	}
+
+private:
+	MsQuicInitializer() {
+		LOG_FUNCTION_ENTER("MsQuicInitializer()");
+	}
+	~MsQuicInitializer() {
+		LOG_FUNCTION_ENTER("~MsQuicInitializer()");
+		//listContexts.~list();
+	}
+
+private:
+	LOG_FIELD_DESTRUCTION(MsQuicInitializer);
+	std::list<Context> listContexts; // use list to avoid invalidation items when push
+};
+
+
+class MyClass {
+public:
+	static int instanceCounter;
+
+	MyClass(int callStaticFunctionAfterMs = 0) {
+		LOG_FUNCTION_ENTER("MyClass() [ptr = {}]", static_cast<void*>(this));
+		auto currentInstanceIdx = instanceCounter++;
+
+		auto contextPtr = MsQuicInitializer::GetInstance().AddContextWithToken(this, token);
+		
+		if (callStaticFunctionAfterMs > 0) {
+			H::Timer::Once(std::chrono::milliseconds{ callStaticFunctionAfterMs }, [contextPtr, currentInstanceIdx] {
+				LOG_FUNCTION_ENTER("Timer_lambda() [{}]", currentInstanceIdx);
+				MyClass::StaticFunction(contextPtr);
+				});
+		}
+	}
+	~MyClass() {
+		LOG_FUNCTION_ENTER("~MyClass()");
+	}
+
+	static void StaticFunction(void* context) {
+		LOG_FUNCTION_ENTER("StaticFunction(context)");
+		auto _context = static_cast<MsQuicInitializer::Context*>(context);
+		if (_context->token.expired()) {
+			_context->shutdownConnectionCallback();
+		}
+	}
+
+private:
+	std::shared_ptr<int> token = std::make_shared<int>();
+};
+
+int MyClass::instanceCounter = 0;
+
+void TestWeakDestroyInSingleton() {
+	//{
+		//MyClass* myClass1 = new MyClass;
+		//MyClass* myClass2 = new MyClass;
+		//MyClass* myClass3 = new MyClass;
+	//}
+	{
+		for (int i = 0; i < 5; i++) {
+			if (i % 3 == 0) {
+				//MyClass myClass(2190);
+				MyClass myClass(1000);
+			}
+			else {
+				MyClass myClass;
+			}
+		}
+		//MyClass myClass1(1000);
+		//MyClass myClass2;
+		//MyClass myClass3(2190);
+		//MyClass myClass4;
+		//MyClass myClass6;
+		//MyClass myClass7(500);
+		//MyClass myClass8;
+		//MyClass myClass1(3000ms);
+	}
+	//{ 
+	//	MyClass myClass2; 
+	//}
+	//{
+		//MyClass myClass3;
+	//}
+	Sleep(2200);
+	return;
+}
+
 
 void TestRemoveExpiredWeakFromVector() {
 	std::vector<std::weak_ptr<int>> vecWeaks;
@@ -133,9 +263,12 @@ void TestRemoveExpiredWeakFromVector() {
 
 
 void main() {
+	lg::DefaultLoggers::Init("D:\\main_26_weak_ptr.log", lg::InitFlags::DefaultFlags);
 	//TestWeakAsToken();
 	//TestWeakOnEmptySharedOrEmptyPointer();
 	//TestWeakAndDestuctorShared();
-	TestRemoveExpiredWeakFromVector();
+	TestWeakDestroyInSingleton();
+	//TestRemoveExpiredWeakFromVector();
+
 	return;
 }
