bdb223b8af06673a3dca9fbc6a21e32c06c1ca05
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-01-19 12:03:07 +0200

    Change main_35_enum_raflection

--- Console/main_35_enum_raflection.cpp
+++ Console/main_35_enum_raflection.cpp
@@ -1,15 +1,46 @@
-#include <map>
-#include <string>
+#include <MagicEnum/MagicEnum.h>
+#include <Helpers/Flags.h>
 #include <iostream>
+#include <string>
+#include <map>
 
-enum class Color {
-	Red,
-	Green,
-	Blue,
+enum EnumA {
+	A1 = 0x01,
+	A2 = 0x02,
+	A3 = 0x04,
+	A4 = 0x08,
 };
 
-// TODO: implement helpers to convert enum to string (as constexpr if it possible)
-
 void main() {
-	
+	auto resAuto = EnumA::A1 | EnumA::A3; // 5
+	EnumA resEnumA_fromInt = static_cast<EnumA>(resAuto); // (A1 | A3) (5)
+	EnumA resEnumA_explicitly = static_cast<EnumA>(EnumA::A1 | EnumA::A2); // (A1 | A3) (5)
+	EnumA copyEnumA = resEnumA_fromInt; // (A1 | A3) (5)
+	EnumA copyEnumA_ = resEnumA_explicitly; // (A1 | A3) (5)
+
+	H::Flags<EnumA> flagsA = EnumA::A1 | EnumA::A3;
+	EnumA resEnumA_from_flagsA = flagsA.ToEnum(); // (A1 | A3) (5)
+	EnumA resEnumA_from_cast_flagsA = flagsA; // (A1 | A3) (5)
+
+	if (flagsA & EnumA::A1) {
+	}
+
+	auto enumA_name = magic_enum::enum_name(resEnumA_from_cast_flagsA); // 0 (because out of bound)
+
+	flagsA.ProcessAllFlags([](EnumA enumA) {
+		switch (enumA) {
+		case EnumA::A1:
+			break;
+		case EnumA::A2:
+			break;
+		case EnumA::A3:
+			break;
+		case EnumA::A4:
+			break;
+		default:
+			int yyy = 0;
+		}
+		return false;
+		});
+
 }
