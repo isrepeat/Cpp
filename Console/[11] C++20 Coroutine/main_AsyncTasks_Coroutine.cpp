2224456defb109d42386ddcab9679d4b3aa5e0f2
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-02-04 23:59:55 +0200

    Adapt main_AsyncTasks_Coroutine to Helpers

--- Console/[11] C++20 Coroutine/main_AsyncTasks_Coroutine.cpp
+++ Console/[11] C++20 Coroutine/main_AsyncTasks_Coroutine.cpp
@@ -5,92 +5,229 @@
 #include <iostream>
 
 H::ConcurrentQueue<H::TaskItemWithDescription> workQueue;
-H::Async::AsyncTasks asyncTasks{ L"asyncTasks" };
+//H::Async::AsyncTasks asyncTasks{ L"asyncTasks" };
 
-//
-//H::Async::AsyncTasks::Task::Ret_t EmbeddedTask() {
-//    LOG_FUNCTION_SCOPE("EmbeddedTask()");
+//H::Async::AsyncTasks::Task::Ret_t DeferredTask_AAA(std::wstring coroFrameName) {
+//    LOG_FUNCTION_SCOPE(L"DeferredTask_AAA({})", coroFrameName);
 //    static thread_local std::size_t functionEnterThreadId = HELPERS_NS::GetThreadId();
 //    using currentTask_t = H::Async::AsyncTasks::Task;
 //
-//    LOG_DEBUG_D("EmbeddedTask computes 1  ~500ms ...");
-//    Sleep(500);
-//
-//    co_await H::Async::ResumeAfter(1000ms, [](std::weak_ptr<currentTask_t> currentTaskWeak) {
-//        LOG_FUNCTION_SCOPE("EmbeddedTask(...)::<timeout_lambda>");
-//        auto currentTask = currentTaskWeak.lock();
-//        if (!currentTask) {
-//            LOG_ERROR_D("currentTask expired");
-//            return;
-//        }
-//        currentTask->resume();
-//        int xxx = 9;
-//        });
-//
-//    LOG_DEBUG_D("EmbeddedTask computes 2  ~500ms ...");
-//    Sleep(500);
-//    co_return;
-//}
-
-//H::Async::CoTask<H::Async::PromiseDefault>::Ret_t SimpleTask(std::wstring coroFrameName) {
-//    LOG_FUNCTION_SCOPE("SimpleTask(coroFrameName)");
 //    LOG_DEBUG_D("Some heavy computes ~1s ...");
 //    Sleep(1000);
+//    
+//    LOG_DEBUG_D("await TaskWithSignal ...");
+//    //co_await H::Async::ResumeAfter<currentTask_t::promise_type>(2000ms);
+//    //co_await *SimpleTask(L"SimpleTask");
+//    co_await *TaskWithResumeSignal(L"TaskSignal", std::make_shared<H::Signal>());
+//    LOG_DEBUG_D("TaskWithSignal finished");
+//
+//    if (HELPERS_NS::GetThreadId() != functionEnterThreadId) {
+//        LOG_ERROR_D("It seems you resume this task from thread that differs from initial. Force return.");
+//        co_return;
+//    }
+//    co_return;
+//}
+//
+//H::Async::CoTask<H::Async::PromiseDefault>::Ret_t TaskSimple(std::wstring coroFrameName) {
+//    LOG_FUNCTION_SCOPE(L"TaskSimple(name = {})", coroFrameName);
 //    co_return;
 //}
 
-H::Async::CoTask<H::Async::PromiseSignal>::Ret_t TaskWithResumeSignal(std::wstring coroFrameName, std::shared_ptr<H::Signal> resumeSignal) {
-    LOG_FUNCTION_SCOPE(L"TaskWithSignal({}, resumeSignal)", coroFrameName);
+
+void AllocateMemoryOnStack() {
+    LOG_FUNCTION_ENTER("AllocateMemoryOnStack()");
+    char array[10'000];
+}
+
+struct Functor {
+    int xx = 9;
+    H::Async::CoTask<H::Async::PromiseDefault>::Ret_t operator()(std::wstring) {
+        co_return;
+    }
+};
+
+H::Async::CoTask<H::Async::PromiseDefault>::Ret_t TaskWithAsyncOperation(std::wstring coroFrameName) {
+    LOG_FUNCTION_SCOPE(L"TaskWithAsyncOperation(name = {})", coroFrameName);
+
     
     co_await H::Async::AsyncOperationWithResumeSignal([](std::weak_ptr<H::Signal> resumeSignalWeak) {
         LOG_FUNCTION_SCOPE(L"AsyncOperationWithSignal__lambda()");
 
-        H::Timer::Once(5000ms, [resumeSignalWeak] { 
+        H::Timer::Once(2000ms, [resumeSignalWeak] { // (1)
             auto resumeSignal = resumeSignalWeak.lock();
             if (!resumeSignal) {
                 LOG_ERROR_D("resumeSignal expired");
                 return;
             }
-            (*resumeSignal)();
-            int xx = 9;
+            (*resumeSignal)(); // (2)
+            NOOP;
             });
         });
-
-    LOG_DEBUG_D("Got response");
+    
+    LOG_DEBUG_D("Got response"); // (3)
     co_return;
 }
 
-H::Async::AsyncTasks::Task::Ret_t DeferredTask_AAA(std::wstring coroFrameName) {
-    LOG_FUNCTION_SCOPE(L"DeferredTask_AAA({})", coroFrameName);
-    static thread_local std::size_t functionEnterThreadId = HELPERS_NS::GetThreadId();
-    using currentTask_t = H::Async::AsyncTasks::Task;
 
-    LOG_DEBUG_D("Some heavy computes ~1s ...");
-    Sleep(1000);
-    
-    LOG_DEBUG_D("await TaskWithSignal ...");
-    //co_await H::Async::ResumeAfter<currentTask_t::promise_type>(2000ms);
-    //co_await *SimpleTask(L"SimpleTask");
-    co_await *TaskWithResumeSignal(L"TaskSignal", std::make_shared<H::Signal>());
-    LOG_DEBUG_D("TaskWithSignal finished");
+struct Temp {
+    Temp() {
+        NOOP;
+    }
+    ~Temp() {
+        NOOP;
+    }
 
-    if (HELPERS_NS::GetThreadId() != functionEnterThreadId) {
-        LOG_ERROR_D("It seems you resume this task from thread that differs from initial. Force return.");
+    void Test() {
+        for (int i = 0; i < 10'000; i++) {
+            array[i] = i;
+        }
+    }
+ 
+    int array[10'000];
+};
+
+class Prototype {
+public:
+    Prototype() {
+        asyncTasks.SetResumeCallback([](std::weak_ptr<H::Async::CoTaskBase> taskWeak) {
+            workQueue.Push({ "signal ResumeNextCoTask()", [taskWeak] {
+                H::Async::SafeResume(taskWeak);
+                NOOP;
+                } });
+            });
+
+
+        LOG_DEBUG("============================================================================");
+        LOG_DEBUG("============================================================================\n\n");
+
+        {
+            auto temp = std::make_shared<Temp>();
+
+            asyncTasks.AddTaskLambda(0ms, [this, temp](std::wstring taskName = {}) -> H::Async::AsyncTasks::Task::Ret_t {
+                LOG_FUNCTION_SCOPE("Task_1__lambda()");
+                NOOP;
+                temp->Test();
+                asyncTasks.Cancel();
+                co_return;
+                }, L"Task_1");
+        }
+        NOOP;
+
+
+        {
+            auto temp = std::make_shared<Temp>();
+
+            asyncTasks.AddTaskLambda(0ms, [temp](std::wstring taskName = {}) -> H::Async::AsyncTasks::Task::Ret_t {
+                LOG_FUNCTION_SCOPE("Task_1__lambda()");
+                NOOP;
+                temp->Test();
+                //asyncTasks.Cancel();
+                co_return;
+                }, L"Task_1");
+        }
+        NOOP;
+
+
+        //auto lambdaA = [this](std::wstring taskName = {}) -> H::Async::AsyncTasks::Task::Ret_t {
+        //    LOG_FUNCTION_SCOPE("Task_1__lambda()");
+        //    NOOP;
+        //    //asyncTasks.Cancel();
+        //    co_return;
+        //};
+
+        //
+        //using R = H::FunctionTraits<decltype(lambdaA)>::Ret;
+        //using C = H::FunctionTraits<decltype(lambdaA)>::Class;
+
+        //std::function<R(std::wstring)> fn = lambdaA;
+        //std::list<decltype(fn)> list;
+        //list.emplace_back(std::move(fn));
+        //list.erase(
+        ////std::set<decltype(lambdaA)> functionsKeeper;
+        //std::unordered_set<decltype(lambdaA)> functionsKeeper;
+        ////std::unordered_set<decltype(fn)> functionsKeeper;
+        ////std::unordered_set<std::function<R(std::wstring)>> functionsKeeper;
+        ////std::pair<std::set<std::basic_string<XCHAR>>::iterator, bool> retIterator;
+        //auto insertResult = functionsKeeper.insert(std::move(lambdaA));
+        //assert(insertResult.first != functionsKeeper.end());
+        //
+
+
+        //asyncTasks.AddTaskFn(0ms, &TaskWithAsyncOperation, L"Task_Signal");
+        ////asyncTasks.AddTaskFn(0ms, this, &Prototype::TaskMember, L"taskSignal");
+        //
+        ////auto xx = asyncTasks.AddTaskLambda(0ms, [](std::wstring) -> H::Async::CoTask<H::Async::PromiseDefault>::Ret_t {
+        //////auto xx = asyncTasks.AddTaskLambda(0ms, [](std::wstring, std::function<void(std::weak_ptr<H::Async::CoTaskBase>)>) -> H::Async::CoTask<H::Async::PromiseRoot>::Ret_t {
+        ////        co_return;
+        ////        });
+
+
+        //asyncTasks.AddTaskLambda(0ms, [this](std::wstring taskName = {}) -> H::Async::AsyncTasks::Task::Ret_t {
+        //    LOG_FUNCTION_SCOPE("Task_Checker__lambda()");
+        //    asyncTasks.Cancel();
+        //    co_return;
+        //    }, L"Task_Checker");
+
+
+        //asyncTasks.AddTaskLambda(0ms, [](std::wstring taskName = {}) -> H::Async::AsyncTasks::Task::Ret_t {
+        //    LOG_FUNCTION_SCOPE("Task_2__lambda()");
+        //    co_return;
+        //    }, L"Task_2");
+
+
+        workQueue.Push({ "signal ShowEvent()", [this] {
+            LOG_DEBUG_D("ShowEvent_Action_1");
+            Sleep(250);
+
+            asyncTasks.StartExecuting();
+            //asyncTasks.StartExecuting(); // will be ignored if coroutine not finished
+
+            Sleep(250);
+            LOG_DEBUG_D("ShowEvent_Action_2");
+            Sleep(500);
+
+            //workQueue.Push({ "signal CustomSlot()", [] {
+            //    LOG_DEBUG_D("CustomSlot_Action_1");
+            //    Sleep(500);
+            //    LOG_DEBUG_D("CustomSlot_Action_2");
+            //    Sleep(500);
+            //    LOG_DEBUG_D("CustomSlot_Action_last");
+            //    Sleep(500);
+            //    return;
+            //    } });
+
+            LOG_DEBUG_D("ShowEvent_Action_last");
+            Sleep(500);
+            return;
+            } });
+    }
+
+    H::Async::CoTask<H::Async::PromiseDefault>::Ret_t TaskMember(std::wstring coroFrameName) {
+        LOG_FUNCTION_SCOPE(L"TaskWithAsyncOperation(name = {})", coroFrameName);
+
+
+        co_await H::Async::AsyncOperationWithResumeSignal([](std::weak_ptr<H::Signal> resumeSignalWeak) {
+            LOG_FUNCTION_SCOPE(L"AsyncOperationWithSignal__lambda()");
+
+            H::Timer::Once(2000ms, [resumeSignalWeak] { // (1)
+                auto resumeSignal = resumeSignalWeak.lock();
+                if (!resumeSignal) {
+                    LOG_ERROR_D("resumeSignal expired");
+                    return;
+                }
+                (*resumeSignal)(); // (2)
+                NOOP;
+                });
+            });
+
+        LOG_DEBUG_D("Got response"); // (3)
         co_return;
     }
-    co_return;
-}
 
-H::Async::AsyncTasks::Task::Ret_t DeferredTask_BBB(std::wstring coroFrameName) {
-    LOG_FUNCTION_SCOPE("DeferredTask_BBB(coroFrameName)");
-    LOG_DEBUG_D("Some heavy computes ~1s ...");
-    Sleep(1000);
-    co_return;
-}
+private:
+    H::Async::AsyncTasks asyncTasks{ L"asyncTasks" };
+};
 
-//H::Async::CoTask<H::Async::PromiseRoot>::Ret_t CustomTask(std::wstring coroFrameName) { 
-//    co_return;
-//}
 
 
 int main() {
@@ -98,48 +235,8 @@ int main() {
 	lg::DefaultLoggers::Init("D:\\main_AsyncTasks_Coroutine.log", lg::InitFlags::DefaultFlags | lg::InitFlags::EnableLogToStdout);
     LOG_FUNCTION_SCOPE("main()");
 
-    asyncTasks.SetResumeCallback([](std::weak_ptr<H::Async::CoTaskBase> rootTaskWeak) {
-        workQueue.Push({ "signal ResumeNextCoTask()", [rootTaskWeak] {
-            auto rootTask = rootTaskWeak.lock();
-            if (!rootTask) {
-                LOG_ERROR_D("rootTask expired");
-                return;
-            }
-            rootTask->resume();
-            int xx = 9;
-            } });
-        });
 
-    asyncTasks.Add(TaskWithResumeSignal(L"TaskSignal", std::make_shared<H::Signal>()), 100ms);
-    //asyncTasks.Add(DeferredTask_AAA(L"AAA"), 100ms);
-    //asyncTasks.Add(DeferredTask_BBB(L"BBB"), 0ms);
-
-    workQueue.Push({ "signal ShowEvent()", [] {
-        LOG_DEBUG_D("ShowEvent_Action_1");
-        Sleep(250);
-
-        asyncTasks.StartExecuting();
-        //asyncTasks.StartExecuting(); // will be ignored if coroutine not finished
-
-        Sleep(250);
-        LOG_DEBUG_D("ShowEvent_Action_2");
-        Sleep(500);
-
-        workQueue.Push({ "signal CustomSlot()", [] {
-            LOG_DEBUG_D("CustomSlot_Action_1");
-            Sleep(500);
-            LOG_DEBUG_D("CustomSlot_Action_2");
-            Sleep(500);
-            LOG_DEBUG_D("CustomSlot_Action_last");
-            Sleep(500);
-            return;
-            } });
-
-        LOG_DEBUG_D("ShowEvent_Action_last");
-        Sleep(500);
-        return;
-        } });
-    
+    Prototype prototype;
 
     while (workQueue.IsWorking()) {
         auto item = workQueue.Pop();
@@ -148,6 +245,5 @@ int main() {
             item.task();
         }
     }
-
 	return 0;
 }
