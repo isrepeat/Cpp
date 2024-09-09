a6bf1d3d0aa289c98aba12e06a62d2de178fbe6d
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2024-09-09 10:49:18 +0300

    Add test nuget namespace conflicts code to Temp.App.

--- TEST/Temp.Dynamic/MyClass_Dynamic.cpp
+++ TEST/Temp.Dynamic/MyClass_Dynamic.cpp
@@ -1,14 +1,27 @@
 #define __MAKE_DYNAMIC_LIBRARY__
 #include "MyClass_Dynamic.h"
 
-//#include <Spdlog/LogHelpers.h>
-#define LOGGER_API
-//#define LOGGER_API __declspec(dllimport) 
-//#define LOGGER_API __declspec(dllexport) 
-#include <Spdlog/LogHelpers.h>
+//#define HELPERS_INLINE_NS Dynamic
+#include "../Temp.Static/MyClass_Static_copy.h"
+//#undef HELPERS_INLINE_NS
 
 
-void DLL_API MethodFromDynamicLibrary() {
-	lg::DefaultLoggers::Init("");
-	int xx = 9;
+namespace NUGET_NS {
+	inline namespace NUGET_INLINE_NS {
+		MyClass::MyClass()
+			: data{new __Helpers_ns::Static::MyTemplate<int>{} }
+		{
+			Helpers::MyClass myClassStatic;
+			//NugetHelpers::MyTemplate<int>::templateNotes;
+			//NugetHelpers::MyTemplate<int> myTemplateStatic;
+		}
+
+		//MyClass::_Data GetData() {
+		//	return __Helpers_ns::Static::MyTemplate<int>{};
+		//}
+
+		void DLL_API SomeFunction() {
+			MyClass myClass;
+		}
+	}
 }
