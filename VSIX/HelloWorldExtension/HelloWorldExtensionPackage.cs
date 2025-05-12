0eefa3cb98d55748d3273aa3e1edb1fcce92e405
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-05-12 07:17:03 +0300

    Add HelloWorldExtension project

---
+++ VSIX/HelloWorldExtension/HelloWorldExtensionPackage.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace HelloWorldExtension {
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(HelloWorldExtensionPackage.PackageGuidString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class HelloWorldExtensionPackage : AsyncPackage {
+        public const string PackageGuidString = "0089fa56-dad3-49b1-a8c5-c290ab558d29";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress) {
+            // When initialized asynchronously, the current thread may be a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            await HelloWorldCommand.InitializeAsync(this);
+        }
+    }
+}
