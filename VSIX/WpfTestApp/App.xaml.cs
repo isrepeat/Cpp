467061d20d1af95009695caec73d7563b06e5fed
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-05-25 12:05:14 +0300

    [WpfTestApp] write WpfTestApp.targets to switch between selected main windows

--- VSIX/WpfTestApp/App.xaml.cs
+++ VSIX/WpfTestApp/App.xaml.cs
@@ -1,12 +1,17 @@
-﻿namespace WpfTestApp {
-    using System.Configuration;
-    using System.Data;
-    using System.Windows;
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
 
+namespace WpfTestApp {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            base.OnStartup(e);
+            var window = MainWindowFactory.Create();
+            //var window = new MainWindow();
+            window.Show();
+        }
     }
-
 }
