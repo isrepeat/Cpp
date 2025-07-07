14760d11d9e2eb75b7eb707d85123ebd5aed5d19
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-07-07 21:10:02 +0300

    ok

---
+++ VSIX/TabsManagerExtension/TemlateSelectors/VirtualMenuItemTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace TabsManagerExtension.TemlateSelectors {
+    public class VirtualMenuItemTemplateSelector : Controls.MenuControl.MenuItemTemplateSelectorBase {
+        public DataTemplate? ProjRefCommandTemplate { get; set; }
+
+        public override DataTemplate? SelectTemplate(object item, DependencyObject container) {
+            if (item is Helpers.MenuItemCommand menuItemCommand) {
+                if (menuItemCommand.CommandParameterContext is State.Document.DocumentProjectReferenceInfo projRefEntry) {
+                    return this.ProjRefCommandTemplate;
+                }
+            }
+
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
