f56f27c50e86c22c23917abb229dd747c18bb820
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-08-01 14:11:57 +0300

    [TestCodeAnalyzer] Add TestCodeAnalyzer project

---
+++ VSIX/TestCodeAnalyzer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
+namespace TestCodeAnalyzer {
+    public class Programm {
+        public static void Main() {
+            Programm.TestMultistateBehaviour();
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.ReadKey();
+        }
+
+        private static void TestMultistateBehaviour() {
+            var projectNode = new Tests.MultistateBehaviour.ProjectNode();
+        }
+    }
+}
---
+++ VSIX/TestCodeAnalyzer/Tests/MultistateBehaviour/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Helpers.Attributes;
+
+
+namespace TestCodeAnalyzer.Tests.MultistateBehaviour {
+    public partial class ProjectNode :
+        Helpers.ObservableObject {
+
+        [ObservableProperty(NotifyMethod = "this.Notify")]
+        //[InvalidatableProperty]
+        //[InvalidatableLazyProperty(nameof(this.InitName))]
+        private string _name;
+
+        public void Notify(string prpoName) {
+        }
+
+        public void Invalidate() {
+        }
+
+        public string InitName() {
+            return "";
+        }
+    }
+}
