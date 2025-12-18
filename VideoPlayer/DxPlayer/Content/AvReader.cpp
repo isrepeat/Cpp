4772988b0cee8038056ade8a4b97e9bda3fa1d9f
isrepeat <71270706+isrepeat@users.noreply.github.com>
2025-12-18 12:47:28 +0200

    Implement loop playbak, add LogCurrentException

--- VideoPlayer/DxPlayer/Content/AvReader.cpp
+++ VideoPlayer/DxPlayer/Content/AvReader.cpp
@@ -1,5 +1,75 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "AvReader.h"
+
+#if defined(__cppwinrt) || defined(WINRT_BASE_H)
+#include <winrt/base.h>
+#endif
+
+namespace Diagnostic {
+    inline void LogCurrentException(const wchar_t* where) {
+        LOG_DEBUG_D(L"[EXCEPTION] {}:", where);
+
+        std::exception_ptr exceptionPtr = std::current_exception();
+        if (!exceptionPtr) {
+            LOG_DEBUG_D(L"[EXCEPTION] {}: no active exception.", where);
+            return;
+        }
+
+        try {
+            std::rethrow_exception(exceptionPtr);
+        }
+        catch (const _com_error& comError) {
+            const HRESULT hr = comError.Error();
+            const wchar_t* msgPtr = comError.ErrorMessage();
+
+            LOG_ERROR_D(
+                L"_com_error:\n"
+                L"hr  = 0x{:08x}\n"
+                L"msg = {}\n"
+                , static_cast<unsigned int>(hr)
+                , (msgPtr != nullptr) ? msgPtr : L"(null)"
+            );
+        }
+#if defined(__cppwinrt) || defined(WINRT_BASE_H)
+        catch (const winrt::hresult_error& winrtError) {
+            const HRESULT hr = static_cast<HRESULT>(winrtError.code());
+            const wchar_t* msgPtr = winrtError.message().c_str();
+
+            LOG_ERROR_D(
+                L"winrt::hresult_error:\n"
+                L"hr  = 0x{:08x}\n"
+                L"msg = {}\n",
+                static_cast<unsigned int>(hr),
+                (msgPtr != nullptr) ? msgPtr : L"(null)"
+            );
+        }
+#endif
+#ifdef __cplusplus_winrt
+        catch (Platform::Exception^ cxException) {
+            const HRESULT hr = cxException->HResult;
+            const wchar_t* msgPtr = cxException->Message != nullptr ? cxException->Message->Data() : L"(null)";
+
+            LOG_ERROR_D(
+                L"Platform::Exception:\n"
+                L"hr  = 0x{:08x}\n"
+                L"msg = {}\n",
+                static_cast<unsigned int>(hr),
+                msgPtr
+            );
+        }
+#endif
+        catch (const std::exception& stdException) {
+            LOG_ERROR_D("std::exception:\n"
+                "what = {}\n",
+                stdException.what()
+            );
+        }
+        catch (...) {
+            LOG_ERROR_D("unknown non-std exception");
+        }
+    }
+} // namespace Diagnostic
+
 
 #if ENGINE_TYPE == MEDIA_ENDIGNE_LOGIC
 namespace MFTools {
@@ -136,6 +206,11 @@ AvReader::~AvReader() {
 
 H::Chrono::Hns AvReader::GetSourceDuration() {
 	return this->sourceDuration;
+}
+
+void AvReader::SetLoopPlayback(bool enableLoop) {
+	std::lock_guard lk{ mx };
+	this->loopPlaybackEnabled = enableLoop;
 }
 
 AvReader::_AvSourceStreamManagerSafeObj::_Locked AvReader::GetLockedAvSourceStreamManager() {
@@ -573,12 +648,34 @@ HRESULT AvReader::OnReadSampleAsync(
 	try {
 		if (dwStreamFlags & MF_SOURCE_READERF_ENDOFSTREAM) {
 			LOG_DEBUG_D("END OF STREAM");
-			this->ClearStream(this->avSourceStreamManagerSafeObj->Lock(), dwStreamFlags);
+            this->ClearStream(this->avSourceStreamManagerSafeObj->Lock(), dwStreamFlags);
+
+			if (this->loopPlaybackEnabled) {
+				this->lastSeekPosition = 0_hns;
+				hr = MFTools::SourceReader::SetPosition(this->sourceReader, this->lastSeekPosition);
+				LOG_FAILED(hr);
+
+				if (SUCCEEDED(hr)) {
+                    auto avSourceStreamManager = this->avSourceStreamManagerSafeObj->Lock();
+
+					// Очистим текущие буферы и запросим первые сэмплы заново
+					if (auto activeVideoStream = avSourceStreamManager->GetActiveVideoStream()) {
+						activeVideoStream->ClearSamples();
+						this->RequestNextSampleInternal(avSourceStreamManager, AvStreamType::Video);
+					}
+					if (auto activeAudioStream = avSourceStreamManager->GetActiveAudioStream()) {
+						activeAudioStream->ClearSamples();
+						this->RequestNextSampleInternal(avSourceStreamManager, AvStreamType::Audio);
+					}
+				}
+
+				return hr;
+			}
 		}
 
 		if (!pSample) {
 			LOG_WARNING_D("SAMPLE IS NULL");
-			this->ClearStream(this->avSourceStreamManagerSafeObj->Lock(), dwStreamFlags);
+            this->ClearStream(this->avSourceStreamManagerSafeObj->Lock(), dwStreamFlags);
 			return S_OK;
 		}
 
@@ -616,7 +713,7 @@ HRESULT AvReader::OnReadSampleAsync(
 		return S_OK;
 	}
 	catch (...) {
-		LOG_ERROR_D("catch exception");
+        Diagnostic::LogCurrentException(L"OnReadSampleAsync");
 		Dbreak;
 
 		this->ClearStream(this->avSourceStreamManagerSafeObj->Lock(), dwStreamFlags);
