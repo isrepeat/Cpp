a0cfd82c691be90f8135f689baaf3c3181779501
Alexandr Bondarenko <alexandr.bondarenko@dct.ua>
2025-12-18 15:29:36 +0200

    Make DxPlayer copy

---
+++ VideoPlayer/DxPlayer_copy/Content/Sample3DSceneRenderer.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+#include "../Common/Config.h"
+#if ENGINE_TYPE == DX_PLAYER_RENDER
+#include "..\Common\DeviceResources.h"
+#include "..\Common\StepTimer.h"
+#include "ShaderStructures.h"
+#include "AvReader.h"
+#include <Helpers/Rational.h>
+#include <Helpers/Dx/DxHelpers.h>
+
+#define VIDEO_RENDERER 1
+
+namespace DxPlayer
+{
+	// This sample renderer instantiates a basic rendering pipeline.
+	class Sample3DSceneRenderer
+	{
+	public:
+		Sample3DSceneRenderer(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		void CreateDeviceDependentResources();
+		void CreateWindowSizeDependentResources();
+		void ReleaseDeviceDependentResources();
+
+#if VIDEO_RENDERER
+		void Init(std::unique_ptr<AvReader> avReader);
+#endif
+		void Update(DX::StepTimer const& timer);
+		void Render();
+
+		void StartTracking();
+		void TrackingUpdate(float positionX);
+		void StopTracking();
+		bool IsTracking();
+
+
+	private:
+		void Rotate(float radians);
+#if VIDEO_RENDERER
+		void RenderVideoSample(const std::unique_ptr<MF::MFVideoSample>& videoSample);
+#endif
+
+	private:
+		// Cached pointer to device resources.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+		
+#if VIDEO_RENDERER
+		std::unique_ptr<AvReader> avReader;
+		const H::Rational<double> monitorRefreshRate;
+		const H::Chrono::milliseconds_f monitorRefreshDuration;
+		std::unique_ptr<MF::MFVideoSample> lastRenderedVideoSample;
+		std::chrono::steady_clock::time_point lastRenderedVideoSampleTimePoint;
+#endif
+
+		// Direct2D resources
+		Microsoft::WRL::ComPtr<ID2D1DrawingStateBlock1> m_stateBlock;
+		Microsoft::WRL::ComPtr<ID2D1Effect> m_pScaleEffect;
+		Microsoft::WRL::ComPtr<ID2D1Effect> m_pCenterEffect;
+
+		// Direct3D resources for cube geometry.
+		Microsoft::WRL::ComPtr<ID3D11InputLayout>	m_inputLayout;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>		m_vertexBuffer;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>		m_indexBuffer;
+		Microsoft::WRL::ComPtr<ID3D11VertexShader>	m_vertexShader;
+		Microsoft::WRL::ComPtr<ID3D11PixelShader>	m_pixelShader;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>		m_constantBuffer;
+
+		// System resources for cube geometry.
+		ModelViewProjectionConstantBuffer	m_constantBufferData;
+		uint32	m_indexCount;
+
+		// Variables used with the rendering loop.
+		bool	m_loadingComplete;
+		float	m_degreesPerSecond;
+		bool	m_tracking;
+	};
+}
+#endif
---
+++ VideoPlayer/DxPlayer_copy/Content/SampleFpsTextRenderer.cpp
@@ -0,0 +1,128 @@
+﻿#include "pch.h"
+#include "SampleFpsTextRenderer.h"
+#if ENGINE_TYPE == DX_PLAYER_RENDER
+#include "Common/DirectXHelper.h"
+
+using namespace DxPlayer;
+using namespace Microsoft::WRL;
+
+// Initializes D2D resources used for text rendering.
+SampleFpsTextRenderer::SampleFpsTextRenderer(const std::shared_ptr<DX::DeviceResources>& deviceResources) : 
+	m_text(L""),
+	m_deviceResources(deviceResources)
+{
+	auto dxDevResources = m_deviceResources->Lock();
+	ZeroMemory(&m_textMetrics, sizeof(DWRITE_TEXT_METRICS));
+
+	// Create device independent resources
+	ComPtr<IDWriteTextFormat> textFormat;
+	DX::ThrowIfFailed(
+		dxDevResources->GetDWriteFactory()->CreateTextFormat(
+			L"Segoe UI",
+			nullptr,
+			DWRITE_FONT_WEIGHT_LIGHT,
+			DWRITE_FONT_STYLE_NORMAL,
+			DWRITE_FONT_STRETCH_NORMAL,
+			32.0f,
+			L"en-US",
+			&textFormat
+			)
+		);
+
+	DX::ThrowIfFailed(
+		textFormat.As(&m_textFormat)
+		);
+
+	DX::ThrowIfFailed(
+		m_textFormat->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_NEAR)
+		);
+
+	DX::ThrowIfFailed(
+		dxDevResources->GetD2DFactory()->CreateDrawingStateBlock(&m_stateBlock)
+		);
+
+	CreateDeviceDependentResources();
+}
+
+// Updates the text to be displayed.
+void SampleFpsTextRenderer::Update(DX::StepTimer const& timer)
+{
+	auto dxDevResources = m_deviceResources->Lock();
+
+	// Update display text.
+	uint32 fps = timer.GetFramesPerSecond();
+
+	m_text = (fps > 0) ? std::to_wstring(fps) + L" FPS" : L" - FPS";
+
+	ComPtr<IDWriteTextLayout> textLayout;
+	DX::ThrowIfFailed(
+		dxDevResources->GetDWriteFactory()->CreateTextLayout(
+			m_text.c_str(),
+			(uint32) m_text.length(),
+			m_textFormat.Get(),
+			240.0f, // Max width of the input text.
+			50.0f, // Max height of the input text.
+			&textLayout
+			)
+		);
+
+	DX::ThrowIfFailed(
+		textLayout.As(&m_textLayout)
+		);
+
+	DX::ThrowIfFailed(
+		m_textLayout->GetMetrics(&m_textMetrics)
+		);
+}
+
+// Renders a frame to the screen.
+void SampleFpsTextRenderer::Render()
+{
+	auto dxDevResources = m_deviceResources->Lock();
+	ID2D1DeviceContext* d2dCtx = dxDevResources->GetD2DDeviceContext();
+	Windows::Foundation::Size logicalSize = dxDevResources->GetLogicalSize();
+
+	d2dCtx->SaveDrawingState(m_stateBlock.Get());
+	d2dCtx->BeginDraw();
+
+	// Position on the bottom right corner
+	D2D1::Matrix3x2F screenTranslation = D2D1::Matrix3x2F::Translation(
+		logicalSize.Width - m_textMetrics.layoutWidth,
+		logicalSize.Height - m_textMetrics.height
+		);
+
+	d2dCtx->SetTransform(screenTranslation * dxDevResources->GetOrientationTransform2D());
+
+	DX::ThrowIfFailed(
+		m_textFormat->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_TRAILING)
+		);
+
+	d2dCtx->DrawTextLayout(
+		D2D1::Point2F(0.f, 0.f),
+		m_textLayout.Get(),
+		m_whiteBrush.Get()
+		);
+
+	// Ignore D2DERR_RECREATE_TARGET here. This error indicates that the device
+	// is lost. It will be handled during the next call to Present.
+	HRESULT hr = d2dCtx->EndDraw();
+	if (hr != D2DERR_RECREATE_TARGET)
+	{
+		DX::ThrowIfFailed(hr);
+	}
+
+	d2dCtx->RestoreDrawingState(m_stateBlock.Get());
+}
+
+void SampleFpsTextRenderer::CreateDeviceDependentResources()
+{
+	auto dxDevResources = m_deviceResources->Lock();
+	DX::ThrowIfFailed(
+		dxDevResources->GetD2DDeviceContext()->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &m_whiteBrush)
+		);
+}
+void SampleFpsTextRenderer::ReleaseDeviceDependentResources()
+{
+	m_whiteBrush.Reset();
+}
+#endif
